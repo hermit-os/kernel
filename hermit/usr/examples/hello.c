8ad708cc9dcc739171775320dccad659e8ed1842
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2015-09-12 17:03:45 +0200

    extend demo to print the environment and all program arguments

--- hermit/usr/examples/hello.c
+++ hermit/usr/examples/hello.c
@@ -36,10 +36,10 @@ int main(int argc, char** argv)
 	int i;
 
 	printf("Hello World!!!\n");
-	//for(i=0; environ[i]; i++)
-	//	printf("environ[%d] = %s\n", i, environ[i]);
-	//for(i=0; i<argc; i++)
-	//	printf("argv[%d] = %s\n", i, argv[i]);
+	for(i=0; environ[i]; i++)
+		printf("environ[%d] = %s\n", i, environ[i]);
+	for(i=0; i<argc; i++)
+		printf("argv[%d] = %s\n", i, argv[i]);
 
 	return errno;
 }
