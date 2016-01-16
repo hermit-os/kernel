d95be9b7dbc43ac453e1607a246f53abd0f0eb9f
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2016-01-16 23:45:16 +0100

    add missing copyright statement

--- hermit/tools/netinet/in.h
+++ hermit/tools/netinet/in.h
@@ -1,3 +1,13 @@
+/* 
+ * Written by the Chair for Operating Systems, RWTH Aachen University
+ * 
+ * NO Copyright (C) 2010-2011, Stefan Lankes
+ * consider these trivial macros to be public domain.
+ * 
+ * These functions are distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+
 #ifndef __NETINET_IN_H__
 #define __NETINET_IN_H__
 
