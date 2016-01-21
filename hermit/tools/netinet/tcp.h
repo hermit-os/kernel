3c65ebb4a467f80d6a16a8ffe60170c24502ce03
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2016-01-21 09:38:07 +0100

    add missing header

---
+++ hermit/tools/netinet/tcp.h
@@ -0,0 +1,16 @@
+/* 
+ * Written by the Chair for Operating Systems, RWTH Aachen University
+ * 
+ * NO Copyright (C) 2010-2011, Stefan Lankes
+ * consider these trivial macros to be public domain.
+ * 
+ * These functions are distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+
+#ifndef __NETINET_TCP_H__
+#define __NETINET_TCP_H__
+
+#include <lwip/tcp.h>
+
+#endif /* __NETINET_TCP_H__ */
