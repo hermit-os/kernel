2ec60a76021fa6e9514e7b6fd51afa1f06d83101
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2016-05-31 04:41:16 +0200

    fix the calucaltion of VMA's lower border

--- hermit/kernel/syscall.c
+++ hermit/kernel/syscall.c
@@ -246,7 +246,7 @@ ssize_t sys_sbrk(ssize_t incr)
 
 	// reserve VMA regions
 	if (PAGE_CEIL(heap->end) > PAGE_CEIL(ret))
-		vma_add(PAGE_CEIL(ret+PAGE_SIZE), PAGE_FLOOR(heap->end), VMA_HEAP|VMA_USER);
+		vma_add(PAGE_CEIL(ret), PAGE_FLOOR(heap->end), VMA_HEAP|VMA_USER);
 
 	// allocation and mapping of new pages for the heap
 	// is catched by the pagefault handler
