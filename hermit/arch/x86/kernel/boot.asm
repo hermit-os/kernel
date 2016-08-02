5f8d80cafebb70102d896b316526b770f7768717
Stefan Lankes <stlankes@users.noreply.github.com>
2016-08-02 07:09:49 +0200

    add code description

--- hermit/arch/x86/kernel/boot.asm
+++ hermit/arch/x86/kernel/boot.asm
@@ -22,6 +22,10 @@
 ; ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 ; (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 ; SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+;
+; This is the kernel's entry point for the application processors.
+; HermitCore switches to the protected mode and jump to its kernel.
+; This trampline code is only used by the HemritCore's single kernel version.
 
 KERNEL_STACK_SIZE equ 0x100
 kernel_start equ 0x800000
