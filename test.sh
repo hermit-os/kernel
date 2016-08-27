4314589c42ddce35da5fb88f0c5010c219edcfe3
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2016-08-27 13:37:08 +0200

    add some simple tests, which Travis CI uses to test HermitCore

---
+++ test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# do not use this script
+# it is written only for internal tests via Travis CI
+
+FILES="hermit/usr/tests/hello hermit/usr/tests/hellof hermit/usr/tests/hello++ hermit/usr/tests/thr_hello hermit/usr/tests/jacobi"
+PROXY=hermit/tools/proxy
+
+for f in $FILES; do echo "check $f..."; $PROXY $f; done
