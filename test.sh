46a68ddab042c30d7345157e537b37a3ed788acc
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2016-08-28 11:05:58 +0200

    reduce debug message, remove test case "jacobi"
    - jacobi takes too long for Travis CI

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 # do not use this script
 # it is written only for internal tests via Travis CI
 
-FILES="hermit/usr/tests/hello hermit/usr/tests/hellof hermit/usr/tests/hello++ hermit/usr/tests/thr_hello hermit/usr/tests/jacobi"
+FILES="hermit/usr/tests/hello hermit/usr/tests/hellof hermit/usr/tests/hello++ hermit/usr/tests/thr_hello"
 PROXY=hermit/tools/proxy
 
 for f in $FILES; do echo "check $f..."; $PROXY $f || exit 1; done
