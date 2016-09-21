78bfdc07bd237a8052532f225ec755d6a58ddfd8
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2016-09-21 22:06:41 +0200

    add echo server from http://www.gopl.io as test for Go's IP stack

---
+++ hermit/usr/tests/server.go
@@ -0,0 +1,43 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// The original code was published at http://www.gopl.io, see page 21.
+
+// This is an "echo" server that displays request parameters.
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	fmt.Println("This is an \"echo\" server that displays request parameters.")
+	fmt.Println("Start the server and send a http request to it (e.g. curl http://localhost:8000/help).")
+	fmt.Println("The server uses port 8000. Please open the port by setting the")
+	fmt.Println("environment variable HERMIT_APP_PORT to 8000.")
+
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
+
+//!+handler
+// handler echoes the HTTP request.
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "%s %s %s\n", r.Method, r.URL, r.Proto)
+	for k, v := range r.Header {
+		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+	}
+	fmt.Fprintf(w, "Host = %q\n", r.Host)
+	fmt.Fprintf(w, "RemoteAddr = %q\n", r.RemoteAddr)
+	if err := r.ParseForm(); err != nil {
+		log.Print(err)
+	}
+	for k, v := range r.Form {
+		fmt.Fprintf(w, "Form[%q] = %q\n", k, v)
+	}
+}
+
+//!-handler
