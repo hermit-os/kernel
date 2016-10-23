d31232d82d5d2fe2875c90749acbe466413ee87d
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2016-10-23 21:52:37 +0200

    remove obsolte function

--- hermit/include/hermit/tasks.h
+++ hermit/include/hermit/tasks.h
@@ -134,21 +134,6 @@ int create_kernel_task(tid_t* id, entry_point_t ep, void* args, uint8_t prio);
 int create_kernel_task_on_core(tid_t* id, entry_point_t ep, void* args, uint8_t prio, uint32_t core_id);
 
 
-/** @brief Create a user level task.
- *
- * @param id The value behind this pointer will be set to the new task's id
- * @param sd Valid socket descriptor to a TCP/IP connection
- * @param argv Pointer to arguments array
- * @param prio Desired priority of the new kernel task
- * @param core_id Start the new task on the core with this id
- *
- * @return
- * - 0 on success
- * - -EINVAL (-22) or -ENOMEM (-12) on failure
- */
-int create_user_task_on_core(tid_t* id, const char* fame, char** argv, uint8_t prio, uint32_t core_id);
-
-
 /** @brief Create a thread local storage for the current task
  *
  * @return
