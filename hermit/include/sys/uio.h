e9d3a8fcb1f9cf5d32e38203a24311f728d6c8e6
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2016-11-20 01:20:01 +0100

    add empty dummy file to be more POSIX compliant

---
+++ hermit/include/sys/uio.h
@@ -0,0 +1,18 @@
+/*
+ * Just an empty dummy handler to be POSIX compliant.
+ */
+#ifndef __SYS_UIO_H__
+#define __SYS_UIO_H__
+
+#include <stdlib.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
