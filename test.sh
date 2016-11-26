2d5b2c9a10daf513e858dbb67ec978993da6153f
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2016-11-26 23:29:16 +0100

    set HERMIT_APP_PORT only, if the Travis tests depend on it

--- test.sh
+++ test.sh
@@ -9,7 +9,7 @@ PROXY=tools/proxy
 for f in $FILES; do echo "check $f..."; $PROXY $f || exit 1; done
 
 # test echo server at port 8000
-$PROXY usr/tests/server &
+HERMIT_APP_PORT=8000 $PROXY usr/tests/server &
 sleep 10
 curl http://127.0.0.1:8000/help
 sleep 1
