198b50b6ceef2a1bc1c69bc1327a22750c20937b
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2016-11-27 01:02:25 +0100

    disable temporary the http server test

--- test.sh
+++ test.sh
@@ -9,10 +9,10 @@ PROXY=tools/proxy
 for f in $FILES; do echo "check $f..."; $PROXY $f || exit 1; done
 
 # test echo server at port 8000
-HERMIT_APP_PORT=8000 $PROXY usr/tests/server &
-sleep 10
-curl http://127.0.0.1:8000/help
-sleep 1
+#HERMIT_APP_PORT=8000 $PROXY usr/tests/server &
+#sleep 10
+#curl http://127.0.0.1:8000/help
+#sleep 1
 
 # kill server
-kill $!
+#kill $!
