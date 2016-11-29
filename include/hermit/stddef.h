828e5070d8ce8beea810e0f4cf8fd6521646f37b
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2016-11-30 00:11:58 +0100

    add auto creation of config.h

--- include/hermit/stddef.h
+++ include/hermit/stddef.h
@@ -42,6 +42,26 @@
 extern "C" {
 #endif
 
+#define TIMER_FREQ	100 /* in HZ */
+#define CLOCK_TICK_RATE	1193182 /* 8254 chip's internal oscillator frequency */
+#define VIDEO_MEM_ADDR	0xB8000 /* the video memory address */
+#define CACHE_LINE	64
+#define HEAP_START	(PAGE_2M_FLOOR((size_t)&kernel_end) + 4*PAGE_SIZE)
+#define HEAP_SIZE	(1ULL << 32)
+#define INT_SYSCALL	0x80
+#define MAILBOX_SIZE	128
+//#define WITH_PCI_IDS
+//#define SAVE_FPU
+//#define CONFIG_VGA
+
+#define BYTE_ORDER             LITTLE_ENDIAN
+
+#define DYNAMIC_TICKS
+
+#define BUILTIN_EXPECT(exp, b)		__builtin_expect((exp), (b))
+//#define BUILTIN_EXPECT(exp, b)	(exp)
+#define NORETURN			__attribute__((noreturn))
+
 #define NULL 		((void*) 0)
 
 /// represents a task identifier
