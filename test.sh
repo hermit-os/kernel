ece7c044b3796d5b4f375eac15f890a9cb725b89
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2017-01-18 15:46:55 +0100

    add test cases for Travis CI

--- test.sh
+++ test.sh
@@ -4,15 +4,20 @@
 # it is written only for internal tests via Travis CI
 
 FILES="usr/tests/hello usr/tests/hellof usr/tests/hello++ usr/tests/thr_hello usr/tests/pi usr/benchmarks/stream usr/benchmarks/basic"
-PROXY=tools/proxy
+PROXY=/opt/hermit/bin/proxy
 
-for f in $FILES; do echo "check $f..."; $PROXY $f || exit 1; done
+if [ "$TRAVIS_BRANCH" = "prodcution" ]
+then
+  echo "No tests on production branch"
+else
+  for f in $FILES; do echo "check $f..."; $PROXY $f || exit 1; done
 
-# test echo server at port 8000
-#HERMIT_APP_PORT=8000 $PROXY usr/tests/server &
-#sleep 10
-#curl http://127.0.0.1:8000/help
-#sleep 1
+  # test echo server at port 8000
+  #HERMIT_APP_PORT=8000 $PROXY usr/tests/server &
+  #sleep 10
+  #curl http://127.0.0.1:8000/help
+  #sleep 1
 
-# kill server
-#kill $!
+  # kill server
+  #kill $!
+fi
