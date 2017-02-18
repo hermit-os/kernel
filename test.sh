4d2adc0f08c0b58f105d526d6ad432070b87dd69
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2017-02-18 11:59:44 +0100

    kill processes after 3min

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@
 FILES="usr/tests/hello usr/tests/hellof usr/tests/hello++ usr/tests/thr_hello usr/tests/pi usr/benchmarks/stream usr/benchmarks/basic"
 PROXY=/opt/hermit/bin/proxy
 
-for f in $FILES; do echo "check $f..."; $PROXY $f || exit 1; done
+for f in $FILES; do echo "check $f..."; timeout -k 180s $PROXY $f || exit 1; done
 
 # test echo server at port 8000
 #HERMIT_APP_PORT=8000 $PROXY usr/tests/server &
