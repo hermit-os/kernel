f583887e59f7fa6e261d62e6c84ea70be61c2fcd
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2017-02-20 22:46:00 +0100

    add http server test

--- test.sh
+++ test.sh
@@ -6,13 +6,15 @@
 FILES="usr/tests/hello usr/tests/hellof usr/tests/hello++ usr/tests/thr_hello usr/tests/pi usr/benchmarks/stream usr/benchmarks/basic"
 PROXY=/opt/hermit/bin/proxy
 
-for f in $FILES; do echo "check $f..."; timeout --kill-after=3m 3m $PROXY $f || exit 1; done
+for f in $FILES; do echo "check $f..."; timeout --kill-after=5m 5m $PROXY $f || exit 1; done
 
 # test echo server at port 8000
-#HERMIT_APP_PORT=8000 $PROXY usr/tests/server &
-#sleep 10
-#curl http://127.0.0.1:8000/help
-#sleep 1
+HERMIT_APP_PORT=8000 $PROXY usr/tests/server &
+sleep 10
+curl http://127.0.0.1:8000/help
+sleep 1
+curl http://127.0.0.1:8000/hello
+sleep 1
 
 # kill server
-#kill $!
+kill $!
