5236ff236e285c929b818eea916539429858b868
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2017-03-04 08:48:32 +0100

    use #if instead of #ifdef to simplify the config file

--- include/hermit/string.h
+++ include/hermit/string.h
@@ -41,35 +41,35 @@
 extern "C" {
 #endif
 
-#ifndef HAVE_ARCH_MEMCPY
+#if !HAVE_ARCH_MEMCPY
 void *memcpy(void *dest, const void *src, size_t count);
 #endif
 
-#ifndef HAVE_ARCH_MEMSET
+#if !HAVE_ARCH_MEMSET
 void *memset(void *dest, int val, size_t count);
 #endif
 
-#ifndef HAVE_ARCH_MEMCMP
+#if !HAVE_ARCH_MEMCMP
 int memcmp(const void *s1, const void *s2, size_t n);
 #endif
 
-#ifndef HAVE_ARCH_STRLEN
+#if !HAVE_ARCH_STRLEN
 size_t strlen(const char *str);
 #endif
 
-#ifndef HAVE_ARCH_STRNCPY
+#if !HAVE_ARCH_STRNCPY
 char *strncpy(char *dest, const char *src, size_t n);
 #endif
 
-#ifndef HAVE_ARCH_STRCPY
+#if !HAVE_ARCH_STRCPY
 char *strcpy(char *dest, const char *src);
 #endif
 
-#ifndef HAVE_ARCH_STRCMP
+#if !HAVE_ARCH_STRCMP
 int strcmp(const char *s1, const char *s2);
 #endif
 
-#ifndef HAVE_ARCH_STRNCMP
+#if !HAVE_ARCH_STRNCMP
 int strncmp(const char *s1, const char *s2, size_t n);
 #endif
 
