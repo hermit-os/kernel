a526cad2e0ecf127019ca3b69a79c2e511fb90cc
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2017-03-09 07:35:59 +0100

    move architecture dependent functions to the subdirectory arch

--- kernel/tasks.c
+++ kernel/tasks.c
@@ -43,9 +43,9 @@
  * Note that linker symbols are not variables, they have no memory allocated for
  * maintaining a value, rather their address is their value.
  */
-extern const void tls_start;
-extern const void tls_end;
 extern atomic_int32_t cpu_online;
+
+volatile uint32_t go_down = 0;
 
 #define TLS_OFFSET	8
 
@@ -252,44 +252,13 @@ int multitasking_init(void)
 	task_table[0].ist_addr = (char*)&boot_ist;
 	set_per_core(kernel_stack, task_table[0].stack + KERNEL_STACK_SIZE - 0x10);
 	set_per_core(current_task, task_table+0);
+#ifdef __i386__
 	set_tss((size_t) task_table[0].stack + KERNEL_STACK_SIZE - 0x10, (size_t) task_table[0].ist_addr + KERNEL_STACK_SIZE - 0x10);
+#endif
 
 	readyqueues[core_id].idle = task_table+0;
 
 	return 0;
-}
-
-
-/* interrupt handler to save / restore the FPU context */
-void fpu_handler(struct state *s)
-{
-	(void) s;
-
-	task_t* task = per_core(current_task);
-	uint32_t core_id = CORE_ID;
-
-	clts(); // clear the TS flag of cr0
-	task->flags |= TASK_FPU_USED;
-
-	if (!(task->flags & TASK_FPU_INIT))  {
-		// use the FPU at the first time => Initialize FPU
-		fpu_init(&task->fpu);
-		task->flags |= TASK_FPU_INIT;
-	}
-
-	if (readyqueues[core_id].fpu_owner == task->id)
-		return;
-
-	spinlock_irqsave_lock(&readyqueues[core_id].lock);
-	// did another already use the the FPU? => save FPU state
-	if (readyqueues[core_id].fpu_owner) {
-		save_fpu_state(&(task_table[readyqueues[core_id].fpu_owner].fpu));
-		task_table[readyqueues[core_id].fpu_owner].flags &= ~TASK_FPU_USED;
-	}
-	readyqueues[core_id].fpu_owner = task->id;
-	spinlock_irqsave_unlock(&readyqueues[core_id].lock);
-
-	restore_fpu_state(&task->fpu);
 }
 
 
@@ -313,7 +282,9 @@ int set_idle_task(void)
 			task_table[i].heap = NULL;
 			readyqueues[core_id].idle = task_table+i;
 			set_per_core(current_task, readyqueues[core_id].idle);
+#ifdef __i386__
 			set_tss((size_t) task_table[i].stack + KERNEL_STACK_SIZE - 0x10, (size_t) task_table[i].ist_addr + KERNEL_STACK_SIZE - 0x10);
+#endif
 			ret = 0;
 
 			break;
@@ -324,39 +295,6 @@ int set_idle_task(void)
 
 	return ret;
 }
-
-
-int init_tls(void)
-{
-	task_t* curr_task = per_core(current_task);
-
-	// do we have a thread local storage?
-	if (((size_t) &tls_end - (size_t) &tls_start) > 0) {
-		char* tls_addr = NULL;
-		size_t fs;
-
-		curr_task->tls_addr = (size_t) &tls_start;
-		curr_task->tls_size = (size_t) &tls_end - (size_t) &tls_start;
-
-		tls_addr = kmalloc(curr_task->tls_size + TLS_OFFSET + sizeof(size_t));
-		if (BUILTIN_EXPECT(!tls_addr, 0)) {
-			LOG_ERROR("load_task: heap is missing!\n");
-			return -ENOMEM;
-		}
-
-		memset(tls_addr, 0x00, TLS_OFFSET);
-		memcpy((void*) (tls_addr+TLS_OFFSET), (void*) curr_task->tls_addr, curr_task->tls_size);
-		fs = (size_t) tls_addr + curr_task->tls_size + TLS_OFFSET;
-		*((size_t*)fs) = fs;
-
-		// set fs register to the TLS segment
-		set_tls(fs);
-		LOG_INFO("TLS of task %d on core %d starts at 0x%zx (size 0x%zx)\n", curr_task->id, CORE_ID, tls_addr + TLS_OFFSET, curr_task->tls_size);
-	} else set_tls(0); // no TLS => clear fs register
-
-	return 0;
-}
-
 
 void finish_task_switch(void)
 {
