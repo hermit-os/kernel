07cd31996ee7bf015ca96a738fc2ec75458b8b29
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2017-05-21 01:05:04 +0200

    disable temporary server test

--- tests.sh
+++ tests.sh
@@ -10,12 +10,12 @@ PROXY=build/local_prefix/opt/hermit/bin/proxy
 for f in $FILES; do echo "check $f..."; HERMIT_ISLE=qemu HERMIT_CPUS=1 HERMIT_KVM=0 HERMIT_VERBOSE=1 timeout --kill-after=5m 5m $PROXY $f || exit 1; done
 
 # test echo server at port 8000
-HERMIT_ISLE=qemu HERMIT_CPUS=1 HERMIT_KVM=0 HERMIT_VERBOSE=1 HERMIT_APP_PORT=8000 $PROXY $TDIR/tests/server &
-sleep 10
-curl http://127.0.0.1:8000/help
-sleep 1
-curl http://127.0.0.1:8000/hello
-sleep 1
+#HERMIT_ISLE=qemu HERMIT_CPUS=1 HERMIT_KVM=0 HERMIT_VERBOSE=1 HERMIT_APP_PORT=8000 $PROXY $TDIR/tests/server &
+#sleep 10
+#curl http://127.0.0.1:8000/help
+#sleep 1
+#curl http://127.0.0.1:8000/hello
+#sleep 1
 
 # kill server
-kill $!
+#kill $!
