6735656b307de32326e15019ffc756efc2d85844
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2017-05-20 15:00:15 +0200

    enable travis tests

--- test.sh
+++ test.sh
@@ -3,13 +3,14 @@
 # do not use this script
 # it is written only for internal tests via Travis CI
 
-FILES="usr/tests/hello usr/tests/hellof usr/tests/hello++ usr/tests/thr_hello usr/tests/pi usr/benchmarks/stream usr/benchmarks/basic usr/tests/signals usr/tests/test-malloc usr/tests/test-malloc-mt"
-PROXY=/opt/hermit/bin/proxy
+TDIR=build/local_prefix/work/hermit/x86_64-hermit/extra
+FILES="$TDIR/tests/hello $TDIR/tests/hellof $TDIR/tests/hello++ $TDIR/tests/thr_hello $TDIR/tests/pi $TDIR/benchmarks/stream $TDIR/benchmarks/basic $TDIR/tests/signals $TDIR/tests/test-malloc $TDIR/tests/test-malloc-mt"
+PROXY=build/local_prefix/work/hermit/bin/proxy/bin/proxy
 
 for f in $FILES; do echo "check $f..."; timeout --kill-after=5m 5m $PROXY $f || exit 1; done
 
 # test echo server at port 8000
-HERMIT_APP_PORT=8000 $PROXY usr/tests/server &
+HERMIT_APP_PORT=8000 $PROXY $TDIR/tests/server &
 sleep 10
 curl http://127.0.0.1:8000/help
 sleep 1
