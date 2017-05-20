7fc46b11e26dc67b7676e5789bb12e167145b423
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2017-05-21 00:13:52 +0200

    disable KVM support

--- tests.sh
+++ tests.sh
@@ -9,13 +9,8 @@ PROXY=build/local_prefix/opt/hermit/bin/proxy
 
 #for f in $FILES; do echo "check $f..."; HERMIT_ISLE=qemu HERMIT_CPUS=1 HERMIT_VERBOSE=1 timeout --kill-after=5m 5m $PROXY $f || exit 1; done
 
-pwd
-ls -la build/local_prefix/
-ls -la build/local_prefix/opt/
-ls -la build/local_prefix/opt/hermit/
-ls -la $TDIR
 # test echo server at port 8000
-HERMIT_ISLE=qemu HERMIT_CPUS=1 HERMIT_VERBOSE=1 HERMIT_APP_PORT=8000 $PROXY $TDIR/tests/server &
+HERMIT_ISLE=qemu HERMIT_CPUS=1 HERMIT_KVM=0 HERMIT_VERBOSE=1 HERMIT_APP_PORT=8000 $PROXY $TDIR/tests/server &
 sleep 10
 curl http://127.0.0.1:8000/help
 sleep 1
