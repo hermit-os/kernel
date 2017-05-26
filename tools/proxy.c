e10b255664c42580a94b10d1b0e526194573c369
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2017-05-27 00:25:11 +0200

    check termination of qemu by permenatly sending SIGINTS

--- tools/proxy.c
+++ tools/proxy.c
@@ -104,10 +104,12 @@ static void qemu_fini(void)
 		unlink(pidname);
 
 		if (id >= 0) {
-			int status = 0;
+			int ret;
 
-			kill(id, SIGINT);
-			wait(&status);
+			do {
+				ret = kill(id, SIGINT);
+				sched_yield();
+			} while((ret < 0) && (errno == ESRCH));
 		}
 	}
 
