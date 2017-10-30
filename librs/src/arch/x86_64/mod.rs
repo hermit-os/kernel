9aa4492e75446ed2a94872b9955360993efe761e
Colin Finck <colin.finck@rwth-aachen.de>
2017-10-30 12:53:03 +0100

    Import Spinlocks, IRQ enable/disable, and processor pausing functions from eduOS-rs

--- librs/src/arch/x86_64/mod.rs
+++ librs/src/arch/x86_64/mod.rs
@@ -21,6 +21,7 @@
 // OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-pub mod processor;
 pub mod gdt;
 pub mod idt;
+pub mod irq;
+pub mod processor;
