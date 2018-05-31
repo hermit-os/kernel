c1d39cfb4bb2140decfe43bba844a508a2c9ac69
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2018-06-01 01:08:33 +0200

    install also libhermit to path all tests

--- tests.sh
+++ tests.sh
@@ -36,8 +36,9 @@ apt-get install -y qemu-system-x86 cmake wget curl gnupg checkinstall gawk dialo
 
 echo "deb [trusted=yes] https://dl.bintray.com/hermitcore/ubuntu bionic main" | tee -a /etc/apt/sources.list
 apt-get -qq update
-apt-get install -y --allow-unauthenticated binutils-hermit newlib-hermit pte-hermit-rs gcc-hermit
+apt-get install -y --allow-unauthenticated binutils-hermit newlib-hermit pte-hermit-rs gcc-hermit libhermit
 export PATH=/opt/hermit/bin:$PATH
+export PATH="$HOME/.cargo/bin:$PATH"
 
 curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain nightly
 cargo --version # dump version of ther Rust toolchain
