3425ab8e7187eb08069857c9d6afeb12b2f3a820
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2018-06-01 07:51:51 +0200

    add check for msr-index and if missing a workaround

--- proxy/uhyve-x86_64.c
+++ proxy/uhyve-x86_64.c
@@ -55,7 +55,59 @@
 #include <sys/eventfd.h>
 #include <linux/const.h>
 #include <linux/kvm.h>
+#ifdef HAVE_MSR_INDEX_H
 #include <asm/msr-index.h>
+#else
+/* x86-64 specific MSRs */
+#define MSR_EFER		0xc0000080 /* extended feature register */
+#define MSR_STAR		0xc0000081 /* legacy mode SYSCALL target */
+#define MSR_LSTAR		0xc0000082 /* long mode SYSCALL target */
+#define MSR_CSTAR		0xc0000083 /* compat mode SYSCALL target */
+#define MSR_SYSCALL_MASK	0xc0000084 /* EFLAGS mask for syscall */
+#define MSR_FS_BASE		0xc0000100 /* 64bit FS base */
+#define MSR_GS_BASE		0xc0000101 /* 64bit GS base */
+#define MSR_KERNEL_GS_BASE	0xc0000102 /* SwapGS GS shadow */
+#define MSR_TSC_AUX		0xc0000103 /* Auxiliary TSC */
+
+#define MSR_IA32_CR_PAT         0x00000277
+#define MSR_PEBS_FRONTEND       0x000003f7
+
+#define MSR_IA32_POWER_CTL      0x000001fc
+
+#define MSR_IA32_MC0_CTL        0x00000400
+#define MSR_IA32_MC0_STATUS     0x00000401
+#define MSR_IA32_MC0_ADDR       0x00000402
+#define MSR_IA32_MC0_MISC       0x00000403
+
+#define MSR_IA32_SYSENTER_CS    0x00000174
+#define MSR_IA32_SYSENTER_ESP   0x00000175
+#define MSR_IA32_SYSENTER_EIP   0x00000176
+
+#define MSR_IA32_APICBASE       0x0000001b
+#define MSR_IA32_APICBASE_BSP   (1<<8)
+#define MSR_IA32_APICBASE_ENABLE (1<<11)
+#define MSR_IA32_APICBASE_BASE  (0xfffff<<12)
+
+#define MSR_IA32_MISC_ENABLE    0x000001a0
+#define MSR_IA32_TSC            0x00000010
+
+/* EFER bits: */
+#define _EFER_SCE               0  /* SYSCALL/SYSRET */
+#define _EFER_LME               8  /* Long mode enable */
+#define _EFER_LMA               10 /* Long mode active (read-only) */
+#define _EFER_NX                11 /* No execute enable */
+#define _EFER_SVME              12 /* Enable virtualization */
+#define _EFER_LMSLE             13 /* Long Mode Segment Limit Enable */
+#define _EFER_FFXSR             14 /* Enable Fast FXSAVE/FXRSTOR */
+
+#define EFER_SCE                (1<<_EFER_SCE)
+#define EFER_LME                (1<<_EFER_LME)
+#define EFER_LMA                (1<<_EFER_LMA)
+#define EFER_NX                 (1<<_EFER_NX)
+#define EFER_SVME               (1<<_EFER_SVME)
+#define EFER_LMSLE              (1<<_EFER_LMSLE)
+#define EFER_FFXSR              (1<<_EFER_FFXSR)
+#endif
 #include <asm/mman.h>
 
 #include "uhyve.h"
