08611e3c26fddd560442c042d20ee2fc914abaef
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2018-06-06 06:40:16 -0500

    remove temporary some tests to see if tests.sh works correctly

--- tests.sh
+++ tests.sh
@@ -53,7 +53,7 @@ cat build/hermit-rs-prefix/src/hermit-rs-stamp/hermit-rs-build-*.log
 fi
 
 TDIR=/work/build/local_prefix/opt/hermit/x86_64-hermit/extra
-FILES="$TDIR/tests/hello $TDIR/tests/hellof $TDIR/tests/hello++ $TDIR/tests/thr_hello $TDIR/tests/pi $TDIR/benchmarks/stream $TDIR/benchmarks/basic $TDIR/tests/signals $TDIR/tests/test-malloc $TDIR/tests/test-malloc-mt $TDIR/tests/argv_envp"
+FILES="$TDIR/tests/hello $TDIR/tests/hellof $TDIR/tests/hello++ $TDIR/tests/thr_hello $TDIR/benchmarks/stream $TDIR/benchmarks/basic $TDIR/tests/signals $TDIR/tests/test-malloc $TDIR/tests/test-malloc-mt $TDIR/tests/argv_envp"
 PROXY=/work/build/local_prefix/opt/hermit/bin/proxy
 
 for f in $FILES; do echo "check $f..."; HERMIT_ISLE=qemu HERMIT_CPUS=1 HERMIT_KVM=0 HERMIT_VERBOSE=1 timeout --kill-after=5m 5m $PROXY $f || exit 1; done
@@ -61,15 +61,15 @@ for f in $FILES; do echo "check $f..."; HERMIT_ISLE=qemu HERMIT_CPUS=1 HERMIT_KV
 for f in $FILES; do echo "check $f..."; HERMIT_ISLE=qemu HERMIT_CPUS=2 HERMIT_KVM=0 HERMIT_VERBOSE=1 timeout --kill-after=5m 5m $PROXY $f || exit 1; done
 
 # test echo server at port 8000
-HERMIT_ISLE=qemu HERMIT_CPUS=1 HERMIT_KVM=0 HERMIT_VERBOSE=1 HERMIT_APP_PORT=8000 $PROXY $TDIR/tests/server &
-sleep 10
-curl http://127.0.0.1:8000/help
-sleep 1
-curl http://127.0.0.1:8000/hello
-sleep 1
+#HERMIT_ISLE=qemu HERMIT_CPUS=1 HERMIT_KVM=0 HERMIT_VERBOSE=1 HERMIT_APP_PORT=8000 $PROXY $TDIR/tests/server &
+#sleep 10
+#curl http://127.0.0.1:8000/help
+#sleep 1
+#curl http://127.0.0.1:8000/hello
+##sleep 1
 
 # kill server
-kill $!
+#kill $!
 
 # test connection via netio
 #wget http://web.ars.de/wp-content/uploads/2017/04/netio132.zip
