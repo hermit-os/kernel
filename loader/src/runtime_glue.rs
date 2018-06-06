afb315bd51382bae9998c798541fbc04a3c06a26
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2018-06-06 05:50:06 -0500

    add support of Rust's new panic handler
    further information at https://users.rust-lang.org/t/psa-breaking-change-panic-fmt-language-item-removed-in-favor-of-panic-implementation/17875

--- loader/src/runtime_glue.rs
+++ loader/src/runtime_glue.rs
@@ -26,14 +26,21 @@
 
 #![allow(private_no_mangle_fns)]
 
+use core::panic::PanicInfo;
+
 #[lang = "eh_personality"]
 extern "C" fn eh_personality() {}
 
-#[lang = "panic_fmt"]
+// see https://users.rust-lang.org/t/psa-breaking-change-panic-fmt-language-item-removed-in-favor-of-panic-implementation/17875
+#[panic_implementation]
 #[no_mangle]
-extern "C" fn panic_fmt(args: ::core::fmt::Arguments, file: &str, line: usize) -> !
-{
-	loaderlog!("PANIC: {}:{}: {}", file, line, args);
+fn panic(info: &PanicInfo) -> ! {
+	if let Some(location) = info.location() {
+		println!("panic occurred in file '{}' at line {}", location.file(), location.line());
+	} else {
+		println!("panic occurred but can't get location information...");
+	}
+
 	loop {
 		unsafe { asm!("hlt" :::: "volatile"); }
 	}
