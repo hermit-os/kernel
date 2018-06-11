b6f38f8253c3021ff1d74dedc90389001bb8f173
Colin Finck <colin.finck@rwth-aachen.de>
2018-06-11 15:25:25 +0200

    Import CMake adaptations from libhermit repo as a first step towards an AArch64 port of HermitCore-rs.

---
+++ cmake/HermitCore-Toolchain-x86_64-bootstrap.cmake
@@ -0,0 +1,9 @@
+include(${CMAKE_CURRENT_LIST_DIR}/HermitCore-Toolchain-x86_64.cmake)
+include_guard()
+
+set(CMAKE_C_COMPILER_WORKS 1 CACHE INTERNAL "")
+set(CMAKE_CXX_COMPILER_WORKS 1 CACHE INTERNAL "")
+
+# bootstrap toolchain cannot compile neither Go nor Fortran
+unset(CMAKE_Go_COMPILER)
+unset(CMAKE_Fortran_COMPILER)
