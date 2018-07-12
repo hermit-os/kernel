ad608ef676ef84374138c28ea51993d643e055c0
Colin Finck <colin.finck@rwth-aachen.de>
2018-07-12 14:13:24 +0200

    Completely build HermitCore-rs without relying on an already installed HermitCore-rs in /opt/hermit.

--- tests.sh
+++ tests.sh
@@ -36,7 +36,7 @@ apt-get install -y qemu-system-x86 cmake wget curl gnupg checkinstall gawk dialo
 
 echo "deb [trusted=yes] https://dl.bintray.com/hermitcore/ubuntu bionic main" | tee -a /etc/apt/sources.list
 apt-get -qq update
-apt-get install -y --allow-unauthenticated binutils-hermit newlib-hermit-rs pte-hermit-rs gcc-hermit-rs libhermit-rs
+apt-get install -y --allow-unauthenticated binutils-hermit newlib-hermit-rs pte-hermit-rs gcc-hermit-rs
 export PATH=/opt/hermit/bin:$PATH
 export PATH="$HOME/.cargo/bin:$PATH"
 
