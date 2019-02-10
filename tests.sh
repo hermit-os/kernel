17a33fac857061b7ea78ac0b1d8b8f6e866e3445
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2019-02-10 12:25:02 +0100

    add scripts to start tests at TravisCI

---
+++ tests.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+OS_NAME=$1
+OS_VERSION=$2
+
+export DEBIAN_FRONTEND="noninteractive"
+
+apt-get -qq update || exit 1
+apt-get install -y --no-install-recommends binutils bsdmainutils ca-certificates cmake curl gcc git libc-dev make nasm qemu-system-x86 rpm || exit 1
+
+echo "deb [trusted=yes] http://dl.bintray.com/hermitcore/ubuntu bionic main" >> /etc/apt/sources.list
+apt-get -qq update || exit 1
+apt-get install -y --allow-unauthenticated binutils-hermit gcc-hermit-rs libomp-hermit-rs newlib-hermit-rs pte-hermit-rs || exit 1
+export PATH=/opt/hermit/bin:$PATH
+export PATH="$HOME/.cargo/bin:$PATH"
+
+curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain nightly
+cargo --version # dump version of the Rust toolchain
+cargo install cargo-xbuild
+rustup component add rust-src
+cargo install --git https://github.com/hermitcore/objmv.git
+cargo install --git https://github.com/hermitcore/pci_ids_parser.git
+make  || exit 1
+
