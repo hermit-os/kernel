7974833a742067c4d5feff7dc628a5ebbec34bf3
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2019-03-16 22:44:55 +0100

    checkin automatically generated file
    => reduce the requirement for the build system

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ use std::process::Command;
 
 fn main() {
 	// create boot code for application processors
-	let _ = Command::new("nasm").args(&["-f", "bin", "-o", "src/arch/x86_64/kernel/boot.bin", "src/arch/x86_64/kernel/boot.asm"]).output().unwrap();
+	/*let _ = Command::new("nasm").args(&["-f", "bin", "-o", "src/arch/x86_64/kernel/boot.bin", "src/arch/x86_64/kernel/boot.asm"]).output().unwrap();
 	let _ = Command::new("sh").args(&["-c", "echo -n \"pub static SMP_BOOT_CODE: [u8; \" > src/arch/x86_64/kernel/smp_boot_code.rs"]).output().unwrap();
 	let _ = Command::new("sh").args(&["-c", "stat -c %s src/arch/x86_64/kernel/boot.bin >> src/arch/x86_64/kernel/smp_boot_code.rs"]).output().unwrap();
 	let _ = Command::new("sh").args(&["-c", "echo -n \"] = [\" >>  src/arch/x86_64/kernel/smp_boot_code.rs"]).output().unwrap();
@@ -10,7 +10,7 @@ fn main() {
 	let _ = Command::new("sh").args(&["-c", "echo -n \"];\" >> src/arch/x86_64/kernel/smp_boot_code.rs"]).output().unwrap();
 
 	// build pci ids as rust file
-	let _ =	Command::new("pci_ids_parser").args(&["src/arch/x86_64/kernel/pci.ids", "src/arch/x86_64/kernel/pci_ids.rs"]).output().unwrap();
+	let _ =	Command::new("pci_ids_parser").args(&["src/arch/x86_64/kernel/pci.ids", "src/arch/x86_64/kernel/pci_ids.rs"]).output().unwrap();*/
 
 	// determine git revision
 	let output = Command::new("git").args(&["rev-parse", "HEAD"]).output().unwrap();
