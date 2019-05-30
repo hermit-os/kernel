bc3dbc28556dc80a661385b860c3772a668aa9ac
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2019-05-30 17:26:48 +0000

    redesign HermitCore tests for libstd

--- librs/examples/src/main.rs
+++ librs/examples/src/main.rs
@@ -1,80 +1,67 @@
-#![feature(test)]
-
-extern crate test;
-
 use std::f64::consts::PI;
+use std::thread;
 
-#[cfg(test)]
-mod tests {
-	use super::*;
-	use std::thread;
-	use test::Bencher;
+fn pi_sequential(num_steps: u64) -> bool {
+	let step = 1.0 / num_steps as f64;
+	let mut sum = 0 as f64;
 
-	fn pi_sequentiel(num_steps: u64) -> f64 {
-		let step = 1.0 / num_steps as f64;
-		let mut sum = 0 as f64;
-
-		for i  in 0..num_steps {
-			let x = (i as f64 + 0.5) * step;
-			sum += 4.0 / (1.0 + x * x);
-		}
-
-		let mypi = sum * (1.0 / num_steps as f64);
-		println!("Pi: {} (sequentiell)", mypi);
-
-		mypi
+	for i  in 0..num_steps {
+		let x = (i as f64 + 0.5) * step;
+		sum += 4.0 / (1.0 + x * x);
 	}
 
-	fn pi_parallel(nthreads: u64, num_steps: u64) -> f64 {
-		let step = 1.0 / num_steps as f64;
-		let mut sum = 0.0 as f64;
+	let mypi = sum * (1.0 / num_steps as f64);
+	println!("Pi: {} (sequential)", mypi);
 
-		let threads: Vec<_> = (0..nthreads)
-		.map(|tid| {
-			thread::spawn(move || {
-				let mut partial_sum = 0 as f64;
-				let start = (num_steps / nthreads) * tid;
-				let end = (num_steps / nthreads) * (tid+1);
+	(mypi - PI).abs() < 0.00001
+}
 
-				for i  in start..end {
-					let x = (i as f64 + 0.5) * step;
-					partial_sum += 4.0 / (1.0 + x * x);
-				}
+fn pi_parallel(nthreads: u64, num_steps: u64) -> bool {
+	let step = 1.0 / num_steps as f64;
+	let mut sum = 0.0 as f64;
 
-				partial_sum
-			})
-		}).collect();
+	let threads: Vec<_> = (0..nthreads)
+	.map(|tid| {
+		thread::spawn(move || {
+			let mut partial_sum = 0 as f64;
+			let start = (num_steps / nthreads) * tid;
+			let end = (num_steps / nthreads) * (tid+1);
 
-		for t in threads {
-			sum += t.join().unwrap();
-		}
+			for i  in start..end {
+				let x = (i as f64 + 0.5) * step;
+				partial_sum += 4.0 / (1.0 + x * x);
+			}
 
-		let mypi = sum * (1.0 / num_steps as f64);
-		println!("Pi: {} (with {} threads)", mypi, nthreads);
+			partial_sum
+		})
+	}).collect();
 
-		mypi
+	for t in threads {
+		sum += t.join().unwrap();
 	}
 
+	let mypi = sum * (1.0 / num_steps as f64);
+	println!("Pi: {} (with {} threads)", mypi, nthreads);
 
-	#[bench]
-	fn bench_pi_sequentiel(b: &mut Bencher) {
-		b.iter(|| pi_sequentiel(50000000));
-	}
+	(mypi - PI).abs() < 0.00001
+}
 
-	#[test]
-	fn pi_sequentiel_test() {
-		let mypi = pi_sequentiel(50000000);
+fn hello() -> bool {
+	println!("Hello, world!");
 
-		assert!((mypi - PI).abs() < 0.00001);
-	}
+    true
+}
 
-	#[test]
-	fn hello() {
-		println!("Hello, world!");
-	}
+fn test_result(b: bool) -> &'static str {
+    if b == true {
+        "ok"
+    } else {
+        "failed!"
+    }
 }
 
 fn main() {
-	println!("Hello from HermitCore!");
-	println!("Please use `cargo test --no-run --target x86_64-unknown-hermit' to build HermitCore tests");
+	println!("Test {} ... {}", stringify!(hello), test_result(hello()));
+    println!("Test {} ... {}", stringify!(pi_sequential), test_result(pi_sequential(50000000)));
+    println!("Test {} ... {}", stringify!(pi_parallel), test_result(pi_parallel(2, 50000000)));
 }
