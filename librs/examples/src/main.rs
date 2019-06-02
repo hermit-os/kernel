749b25d065a9b9d453c38e69c07971350c154550
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2019-06-02 10:00:29 +0200

    increase the readability

--- librs/examples/src/main.rs
+++ librs/examples/src/main.rs
@@ -49,39 +49,39 @@ fn pi_parallel(nthreads: u64, num_steps: u64) -> bool {
 fn hello() -> bool {
 	println!("Hello, world!");
 
-    true
+	true
 }
 
 fn threading() -> bool {
-    // Make a vector to hold the children which are spawned.
-    let mut children = vec![];
+	// Make a vector to hold the children which are spawned.
+	let mut children = vec![];
 
-    for i in 0..2 {
-        // Spin up another thread
-        children.push(thread::spawn(move || {
-            println!("this is thread number {}", i);
-        }));
-    }
+	for i in 0..2 {
+		// Spin up another thread
+		children.push(thread::spawn(move || {
+			println!("this is thread number {}", i);
+		}));
+	}
 
-    for child in children {
-        // Wait for the thread to finish. Returns a result.
-        let _ = child.join();
-    }
+	for child in children {
+		// Wait for the thread to finish. Returns a result.
+		let _ = child.join();
+	}
 
-    true
+	true
 }
 
 fn test_result(b: bool) -> &'static str {
-    if b == true {
-        "ok"
-    } else {
-        "failed!"
-    }
+	if b == true {
+		"ok"
+	} else {
+		"failed!"
+	}
 }
 
 fn main() {
 	println!("Test {} ... {}", stringify!(hello), test_result(hello()));
-    println!("Test {} ... {}", stringify!(threading), test_result(threading()));
-    println!("Test {} ... {}", stringify!(pi_sequential), test_result(pi_sequential(50000000)));
-    println!("Test {} ... {}", stringify!(pi_parallel), test_result(pi_parallel(2, 50000000)));
+	println!("Test {} ... {}", stringify!(threading), test_result(threading()));
+	println!("Test {} ... {}", stringify!(pi_sequential), test_result(pi_sequential(50000000)));
+	println!("Test {} ... {}", stringify!(pi_parallel), test_result(pi_parallel(2, 50000000)));
 }
