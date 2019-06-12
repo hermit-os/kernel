b0c0f8fe7a25ec4b54377254365cd7e9006f4d15
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2019-06-12 23:43:14 +0200

    revise driver, disable interrupts during intialization

--- librs/src/drivers/net/uhyve.rs
+++ librs/src/drivers/net/uhyve.rs
@@ -5,7 +5,6 @@
 // http://opensource.org/licenses/MIT>, at your option. This file may not be
 // copied, modified, or distributed except according to those terms.
 
-use alloc::vec::Vec;
 use alloc::collections::BTreeMap;
 use core::ptr::read_volatile;
 use core::str;
@@ -19,7 +18,7 @@ use smoltcp::Result;
 use smoltcp::phy::{self, Device, DeviceCapabilities};
 
 use scheduler;
-use drivers::net::NETWORK_TASK_ID;
+use drivers::net::{NETWORK_TASK_ID,NET_SEM};
 
 #[cfg(target_arch="x86_64")]
 use crate::arch::x86_64::kernel::percore::core_scheduler;
@@ -35,9 +34,9 @@ use crate::arch::x86_64::mm::paging::virt_to_phys;
 use x86::io::*;
 
 const UHYVE_IRQ_NET: u32 = 11;
-const UHYVE_PORT_NETINFO: u16   = 0x600;
-const UHYVE_PORT_NETWRITE: u16  = 0x640;
-const UHYVE_PORT_NETREAD: u16   = 0x680;
+const UHYVE_PORT_NETINFO: u16	= 0x600;
+const UHYVE_PORT_NETWRITE: u16	= 0x640;
+const UHYVE_PORT_NETREAD: u16	= 0x680;
 //const UHYVE_PORT_NETSTAT: u16   = 0x700;
 const UHYVE_MAX_MSG_SIZE: usize = 1792;
 
@@ -82,103 +81,120 @@ impl UhyveRead {
 		}
 	}
 }
+
 /// Datatype to forward packets to uhyve
 #[derive(Debug)]
 #[repr(C)]
 struct UhyveWrite {
-    /// address to the data
-    pub data: usize,
-    /// length of the data
-    pub len: usize,
-    /// return value, transfered bytes
-    pub ret: i32
+	/// address to the data
+	pub data: usize,
+	/// length of the data
+	pub len: usize,
+	/// return value, transfered bytes
+	pub ret: i32
 }
 
 impl UhyveWrite {
-    pub fn new(data: usize, len: usize) -> Self {
-        UhyveWrite {
-            data: data,
-            len: len,
-            ret: 0
-        }
-    }
+	pub fn new(data: usize, len: usize) -> Self {
+		UhyveWrite {
+			data: data,
+			len: len,
+			ret: 0
+		}
+	}
 
-    pub fn ret(&self) -> i32 {
-        unsafe {
-            read_volatile(&self.ret)
-        }
-    }
+	pub fn ret(&self) -> i32 {
+		unsafe {
+			read_volatile(&self.ret)
+		}
+	}
+
+	pub fn len(&self) -> usize {
+		unsafe {
+			read_volatile(&self.len)
+		}
+	}
 }
 
 extern "C" fn uhyve_thread(_arg: usize) {
-    debug!("Hello from network thread!");
+	debug!("Hello from network thread!");
 
-    let info: UhyveNetinfo = UhyveNetinfo::default();
+	::arch::irq::disable();
 
-    unsafe {
-        outl(UHYVE_PORT_NETINFO, virt_to_phys(&info as *const _ as usize) as u32);
-    }
-    let mac_str = str::from_utf8(&info.mac).unwrap();
+	let info: UhyveNetinfo = UhyveNetinfo::default();
 
-    let neighbor_cache = NeighborCache::new(BTreeMap::new());
+	unsafe {
+		outl(UHYVE_PORT_NETINFO, virt_to_phys(&info as *const _ as usize) as u32);
+	}
+	let mac_str = str::from_utf8(&info.mac).unwrap();
+
+	let neighbor_cache = NeighborCache::new(BTreeMap::new());
 	let ethernet_addr = EthernetAddress([
-        u8::from_str_radix(&mac_str[0..2], 16).unwrap(),
-        u8::from_str_radix(&mac_str[3..5], 16).unwrap(),
-        u8::from_str_radix(&mac_str[6..8], 16).unwrap(),
-        u8::from_str_radix(&mac_str[9..11], 16).unwrap(),
-        u8::from_str_radix(&mac_str[12..14], 16).unwrap(),
-        u8::from_str_radix(&mac_str[15..17], 16).unwrap()
-    ]);
+		u8::from_str_radix(&mac_str[0..2], 16).unwrap(),
+		u8::from_str_radix(&mac_str[3..5], 16).unwrap(),
+		u8::from_str_radix(&mac_str[6..8], 16).unwrap(),
+		u8::from_str_radix(&mac_str[9..11], 16).unwrap(),
+		u8::from_str_radix(&mac_str[12..14], 16).unwrap(),
+		u8::from_str_radix(&mac_str[15..17], 16).unwrap()
+	]);
 	let hcip = get_ip();
-	let ip_addrs = [
-		IpCidr::new(IpAddress::v4(hcip[0], hcip[1], hcip[2], hcip[3]), 24)
-	];
-    let hcgw = get_gateway();
-    let default_gw = Ipv4Address::new(hcgw[0], hcgw[1], hcgw[2], hcgw[3]);
-    let mut routes_storage = [None; 1];
-    let mut routes = Routes::new(&mut routes_storage[..]);
-    routes.add_default_ipv4_route(default_gw).unwrap();
-    let device = UhyveNet::new();
+	let ip_addrs = [IpCidr::new(IpAddress::v4(hcip[0], hcip[1], hcip[2], hcip[3]), 24)]; 
+	let hcgw = get_gateway();
+	let default_gw = Ipv4Address::new(hcgw[0], hcgw[1], hcgw[2], hcgw[3]);
+	let mut routes_storage = [None; 1];
+	let mut routes = Routes::new(&mut routes_storage[..]);
+	routes.add_default_ipv4_route(default_gw).unwrap();
+	let device = UhyveNet::new();
+
+	info!("MAC address {}", ethernet_addr);
+	info!("Configure network interface with address {}", ip_addrs[0]);
+	info!("Configure gatway with address {}", default_gw);
+
 	let mut iface = EthernetInterfaceBuilder::new(device)
-        .ethernet_addr(ethernet_addr)
+		.ethernet_addr(ethernet_addr)
 		.neighbor_cache(neighbor_cache)
-        .ip_addrs(ip_addrs)
-        .routes(routes)
-        .finalize();
+		.ip_addrs(ip_addrs)
+		.routes(routes)
+		.finalize();
 
-    info!("MAC address {}", ethernet_addr);
-    info!("Configure network interface with address {}", ip_addrs[0]);
-    info!("Configure gatway with address {}", default_gw);
-
-    let mut sockets = SocketSet::new(vec![]);
-    let boot_time = crate::arch::get_boot_time();
+	let mut sockets = SocketSet::new(vec![]);
+	let boot_time = crate::arch::get_boot_time();
 	let mut counter: usize = 0;
 
-    loop {
-		let microseconds = crate::arch::processor::get_timer_ticks() - boot_time;
-        let timestamp = Instant::from_millis(microseconds as i64 / 1000i64);
+	// Install interrupzt handler
+	irq_install_handler(UHYVE_IRQ_NET, uhyve_irqhandler as usize);
 
-        match iface.poll(&mut sockets, timestamp) {
-            Ok(ready) => {
+	::arch::irq::enable();
+
+	loop {
+		let microseconds = ::arch::processor::get_timer_ticks() - boot_time;
+		let timestamp = Instant::from_millis(microseconds as i64 / 1000i64);
+
+		match iface.poll(&mut sockets, timestamp) {
+			Ok(ready) => {
 				if ready == true {
-                	trace!("receive message {}", counter);
+					trace!("receive message {}", counter);
 				} else {
-					crate::drivers::net::NET_SEM.acquire(None);
+					match iface.poll_delay(&sockets, timestamp) {
+						Some(duration) => {
+							trace!("duration {}", duration);
+							NET_SEM.acquire(Some(duration.millis()))
+						},
+						None => NET_SEM.acquire(None),
+					};
 				}
-            },
-            Err(e) => {
-				trace!("poll error: {}", e);
-            }
-        }
+			},
+			Err(e) => {
+				debug!("poll error: {}", e);
+			}
+		}
 
 		counter = counter+1;
-    }
+	}
 }
 
 pub fn init() {
-    irq_install_handler(UHYVE_IRQ_NET, uhyve_irqhandler as usize);
-
-    let core_scheduler = core_scheduler();
+	let core_scheduler = core_scheduler();
 	unsafe {
 		NETWORK_TASK_ID = core_scheduler.spawn(
 			uhyve_thread,
@@ -191,62 +207,62 @@ pub fn init() {
 
 #[cfg(target_arch="x86_64")]
 extern "x86-interrupt" fn uhyve_irqhandler(_stack_frame: &mut ExceptionStackFrame) {
-    trace!("Receive network interrupt from uhyve");
-    crate::drivers::net::NET_SEM.release();
+	trace!("Receive network interrupt from uhyve");
+	crate::drivers::net::NET_SEM.release();
 	apic::eoi();
-    core_scheduler().scheduler();
+	core_scheduler().scheduler();
 }
 
 /// A network device for uhyve.
 #[derive(Debug)]
 pub struct UhyveNet {
-    mtu:    usize
+	mtu:	usize
 }
 
 impl UhyveNet {
-    /// Creates a network device for uhyve.
-    ///
-    /// Every packet transmitted through this device will be received through it
-    /// in FIFO order.
-    pub fn new() -> UhyveNet {
-        UhyveNet {
-            mtu: 1500
-        }
-    }
+	/// Creates a network device for uhyve.
+	///
+	/// Every packet transmitted through this device will be received through it
+	/// in FIFO order.
+	pub fn new() -> UhyveNet {
+		UhyveNet {
+			mtu: 1500
+		}
+	}
 }
 
 impl<'a> Device<'a> for UhyveNet {
-    type RxToken = RxToken;
-    type TxToken = TxToken;
+	type RxToken = RxToken;
+	type TxToken = TxToken;
 
-    fn capabilities(&self) -> DeviceCapabilities {
-        let mut cap = DeviceCapabilities::default();
-        cap.max_transmission_unit = self.mtu;
-        cap
-    }
+	fn capabilities(&self) -> DeviceCapabilities {
+		let mut cap = DeviceCapabilities::default();
+		cap.max_transmission_unit = self.mtu;
+		cap
+	}
 
-    fn receive(&'a mut self) -> Option<(Self::RxToken, Self::TxToken)> {
+	fn receive(&'a mut self) -> Option<(Self::RxToken, Self::TxToken)> {
 		let mut rx = RxToken::new();
-        let data = UhyveRead::new(virt_to_phys(rx.buffer.as_mut_ptr() as usize), UHYVE_MAX_MSG_SIZE);
-        unsafe {
-            outl(UHYVE_PORT_NETREAD, virt_to_phys(&data as *const _ as usize) as u32);
-        }
+		let data = UhyveRead::new(virt_to_phys(rx.buffer.as_mut_ptr() as usize), UHYVE_MAX_MSG_SIZE);
+		unsafe {
+			outl(UHYVE_PORT_NETREAD, virt_to_phys(&data as *const _ as usize) as u32);
+		}
 
-        if data.ret() == 0 {
-            let tx = TxToken { };
+		if data.ret() == 0 {
+			let tx = TxToken::new();
 			rx.resize(data.len());
 			trace!("resize message to {} bytes", rx.len());
 
-            Some((rx, tx))
-        } else {
-            None
-        }
-    }
+			Some((rx, tx))
+		} else {
+			None
+		}
+	}
 
-    fn transmit(&'a mut self) -> Option<Self::TxToken> {
+	fn transmit(&'a mut self) -> Option<Self::TxToken> {
 		trace!("create TxToken to transfer data");
-        Some(TxToken {})
-    }
+		Some(TxToken::new())
+	}
 }
 
 #[doc(hidden)]
@@ -277,40 +293,46 @@ impl RxToken {
 }
 
 impl phy::RxToken for RxToken {
-    fn consume<R, F>(self, _timestamp: Instant, f: F) -> Result<R>
+	fn consume<R, F>(self, _timestamp: Instant, f: F) -> Result<R>
 		where F: FnOnce(&[u8]) -> Result<R>
-    {
+	{
 		let (first, _) = self.buffer.split_at(self.len);
-        f(first)
-    }
+		f(first)
+	}
 }
 
 #[doc(hidden)]
 pub struct TxToken;
 
 impl TxToken {
-    fn write(&self, data: usize, len: usize) -> usize {
-        let uhyve_write = UhyveWrite::new(virt_to_phys(data), len);
-        unsafe {
-            outl(UHYVE_PORT_NETWRITE, virt_to_phys(&uhyve_write as *const _ as usize) as u32);
-        }
-        let  ret = uhyve_write.ret() as usize;
+	pub const fn new() -> Self {
+		TxToken {}
+	}
 
-        if ret != len {
-            debug!("Incorrect return value: {} != {}", ret, len);
-        }
+	fn write(&self, data: usize, len: usize) -> usize {
+		let uhyve_write = UhyveWrite::new(virt_to_phys(data), len);
+		unsafe {
+			outl(UHYVE_PORT_NETWRITE, virt_to_phys(&uhyve_write as *const _ as usize) as u32);
+		}
 
-        ret
-    }
+		let  ret = uhyve_write.ret();
+		if ret != 0 {
+			debug!("Unable to send message: {}", ret);
+		}
+
+		uhyve_write.len()
+	}
 }
 
 impl phy::TxToken for TxToken {
-    fn consume<R, F>(self, _timestamp: Instant, len: usize, f: F) -> Result<R>
-        where F: FnOnce(&mut [u8]) -> Result<R>
-    {
-        let mut buffer = Vec::with_capacity(len);
-        let result = f(&mut buffer);
-        self.write(buffer.as_ptr() as usize, len);
-        result
-    }
+	fn consume<R, F>(self, _timestamp: Instant, len: usize, f: F) -> Result<R>
+		where F: FnOnce(&mut [u8]) -> Result<R>
+	{
+		let mut buffer = vec![0; len];
+		let result = f(&mut buffer);
+		if result.is_ok() == true {
+			self.write(buffer.as_ptr() as usize, len);
+		}
+		result
+	}
 }
