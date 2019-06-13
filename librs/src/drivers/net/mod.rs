d7d5cfc704382ea11aaf2d2a8400cde00bbbc4fb
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2019-06-13 12:35:43 +0200

    create a networkd to handle all network requests

--- librs/src/drivers/net/mod.rs
+++ librs/src/drivers/net/mod.rs
@@ -7,6 +7,11 @@
 
 pub mod uhyve;
 
+use smoltcp::time::Instant;
+use smoltcp::socket::SocketSet;
+use smoltcp::iface::EthernetInterface;
+use smoltcp::phy::Device;
+
 use synch::semaphore::*;
 use scheduler::task::TaskId;
 
@@ -16,5 +21,37 @@ static mut NETWORK_TASK_ID: TaskId = TaskId::from(0);
 pub fn get_network_task_id() -> TaskId {
 	unsafe {
 		NETWORK_TASK_ID
+	}
+}
+
+pub fn networkd<'b, 'c, 'e, DeviceT: for<'d> Device<'d>>(iface: &mut EthernetInterface<'b, 'c, 'e, DeviceT>) -> ! {
+	let mut sockets = SocketSet::new(vec![]);
+	let boot_time = crate::arch::get_boot_time();
+	let mut counter: usize = 0;
+
+	loop {
+		let microseconds = ::arch::processor::get_timer_ticks() - boot_time;
+		let timestamp = Instant::from_millis(microseconds as i64 / 1000i64);
+
+		match iface.poll(&mut sockets, timestamp) {
+			Ok(ready) => {
+				if ready == true {
+					trace!("receive message {}", counter);
+				} else {
+					match iface.poll_delay(&sockets, timestamp) {
+						Some(duration) => {
+							trace!("duration {}", duration);
+							NET_SEM.acquire(Some(duration.millis()))
+						},
+						None => NET_SEM.acquire(None),
+					};
+				}
+			},
+			Err(e) => {
+				debug!("poll error: {}", e);
+			}
+		}
+
+		counter = counter+1;
 	}
 }
