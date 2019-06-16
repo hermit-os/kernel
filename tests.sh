0f646f0d7a9fa0d95c1efb7f24ce6770bf717608
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2019-06-16 17:34:47 +0200

    enable crago tests within CI

--- tests.sh
+++ tests.sh
@@ -18,6 +18,7 @@ curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain nightly
 cargo --version # dump version of the Rust toolchain
 cargo install cargo-xbuild
 rustup component add rust-src
+cargo test
 #cargo install --git https://github.com/hermitcore/objmv.git
 #cargo install --git https://github.com/hermitcore/pci_ids_parser.git
 #mkdir build
