698c4f3b8364e1b6c2c284a7688c358e1fc64939
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2019-07-07 23:04:54 +0200

    use panic strategy "abort"

--- src/runtime_glue.rs
+++ src/runtime_glue.rs
@@ -35,6 +35,11 @@ fn panic(info: &PanicInfo) -> ! {
 }
 
 #[cfg(not(test))]
+#[lang = "eh_personality"]
+#[no_mangle]
+pub extern fn eh_personality() {}
+
+#[cfg(not(test))]
 #[lang = "oom"]
 #[no_mangle]
 pub fn rust_oom(layout: Layout) -> ! {
