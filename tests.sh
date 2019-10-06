57a2cdb58f42cab2d2012e3dccd9de21a5b22258
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2019-10-06 11:22:27 +0200

    add tests within qemu

--- tests.sh
+++ tests.sh
@@ -12,3 +12,8 @@ export PATH="$HOME/.cargo/bin:$PATH"
 cargo --version # dump version of the Rust toolchain
 cargo install cargo-xbuild
 cargo test --lib
+make clean
+make all
+make release=1 all
+make qemu
+make release=1 qemu
