bd4edfbf682910cfb5c05ce71a0d7f05ad3fc1ca
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2019-12-16 11:53:54 +0100

    use ubunutu 18.04 to test libhermit

--- tests.sh
+++ tests.sh
@@ -6,14 +6,18 @@ export DEBIAN_FRONTEND="noninteractive"
 apt-get clean
 apt-get -qq update
 
-export PATH=/opt/hermit/bin:$PATH
+# Install required packets from ubuntu repository
+apt-get install -y apt-transport-https curl wget vim nano git binutils autoconf automake make cmake qemu-kvm qemu-system-x86 nasm gcc g++ build-essential libtool bsdmainutils
+
+# Install Rust compiler
+curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain nightly
+
 export PATH="$HOME/.cargo/bin:$PATH"
 
 cargo --version # dump version of the Rust toolchain
-cargo install cargo-xbuild
-cargo test --lib
-make clean
-make all
-make release=1 all
-make qemu
-make release=1 qemu
+rustup component add rust-src
+rustup component add llvm-tools-preview
+
+cargo build -Z build-std=core,alloc --target x86_64-unknown-hermit-kernel
+cargo build -Z build-std=core,alloc --target x86_64-unknown-hermit-kernel --release
+cargo test --target x86_64-unknown-linux-gnu
