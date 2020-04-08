41c3bbde5d010424418a1dd7fad944ed89258114
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2020-04-08 21:32:15 +0200

    remove deadlock by holding the state lock during a wakeup IPI

--- src/scheduler/task.rs
+++ src/scheduler/task.rs
@@ -475,14 +475,10 @@ impl BlockedTaskQueue {
 		let core_scheduler = scheduler::get_scheduler(core_id);
 
 		// Add the task to the ready queue.
-		let is_halted = {
-			let mut state_locked = core_scheduler.state.lock();
-			state_locked.ready_queue.push(task);
-			state_locked.is_halted
-		};
-
-		// Wake up the CPU if needed.
-		if is_halted {
+		let mut state_locked = core_scheduler.state.lock();
+		state_locked.ready_queue.push(task);
+		if state_locked.is_halted {
+			// Wake up the CPU if needed.
 			arch::wakeup_core(core_id);
 		}
 	}
