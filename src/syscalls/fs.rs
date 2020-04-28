aa6ef5064bea5d29b3b8741ce37264733031fc25
Thomas Lambertz <mail@thomaslambertz.de>
2020-04-29 01:40:56 +0200

    Add license headers

--- src/syscalls/fs.rs
+++ src/syscalls/fs.rs
@@ -1,3 +1,10 @@
+// Copyright (c) 2020 Thomas Lambertz, RWTH Aachen University
+//
+// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
+// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+// http://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
+
 use alloc::borrow::ToOwned;
 use alloc::boxed::Box;
 use alloc::collections::BTreeMap;
