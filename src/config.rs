98824b7ea576f7093e49656cb5e2ec73024cc3ce
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2020-05-12 19:58:31 +0200

    avoid unneeded interrupts
    - introduce the flag VRING_AVAIL_F_NO_INTERRUPT
    - introduce the constant VIRTIO_MAX_QUEUE_SIZE to define
      the maximum supported size of virtio queues

--- src/config.rs
+++ src/config.rs
@@ -6,3 +6,6 @@ pub const DEFAULT_STACK_SIZE: usize = 32_768;
 
 #[allow(dead_code)]
 pub const USER_STACK_SIZE: usize = 1_048_576;
+
+#[allow(dead_code)]
+pub const VIRTIO_MAX_QUEUE_SIZE: u16 = 256;
