32ee155559ee84cb90b55d5be2449cfc161fd4fd
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2020-10-05 23:56:37 +0200

    increasing the readability with rustfmt

--- src/arch/x86_64/mm/paging.rs
+++ src/arch/x86_64/mm/paging.rs
@@ -611,7 +611,9 @@ pub fn get_page_table_entry<S: PageSize>(virtual_address: VirtAddr) -> Option<Pa
 	trace!("Looking up Page Table Entry for {:#X}", virtual_address);
 
 	let page = Page::<S>::including_address(virtual_address);
-	let root_pagetable = unsafe { &mut *mem::transmute::<*mut u64, *mut PageTable<PML4>>(PML4_ADDRESS.as_mut_ptr()) };
+	let root_pagetable = unsafe {
+		&mut *mem::transmute::<*mut u64, *mut PageTable<PML4>>(PML4_ADDRESS.as_mut_ptr())
+	};
 	root_pagetable.get_page_table_entry(page)
 }
 
@@ -619,7 +621,9 @@ pub fn get_physical_address<S: PageSize>(virtual_address: VirtAddr) -> PhysAddr 
 	trace!("Getting physical address for {:#X}", virtual_address);
 
 	let page = Page::<S>::including_address(virtual_address);
-	let root_pagetable = unsafe { &mut *mem::transmute::<*mut u64, *mut PageTable<PML4>>(PML4_ADDRESS.as_mut_ptr()) };
+	let root_pagetable = unsafe {
+		&mut *mem::transmute::<*mut u64, *mut PageTable<PML4>>(PML4_ADDRESS.as_mut_ptr())
+	};
 	let address = root_pagetable
 		.get_page_table_entry(page)
 		.expect("Entry not present")
@@ -681,7 +685,9 @@ pub fn map<S: PageSize>(
 	);
 
 	let range = get_page_range::<S>(virtual_address, count);
-	let root_pagetable = unsafe { &mut *mem::transmute::<*mut u64, *mut PageTable<PML4>>(PML4_ADDRESS.as_mut_ptr()) };
+	let root_pagetable = unsafe {
+		&mut *mem::transmute::<*mut u64, *mut PageTable<PML4>>(PML4_ADDRESS.as_mut_ptr())
+	};
 	root_pagetable.map_pages(range, physical_address, flags);
 }
 
@@ -693,7 +699,9 @@ pub fn unmap<S: PageSize>(virtual_address: VirtAddr, count: usize) {
 	);
 
 	let range = get_page_range::<S>(virtual_address, count);
-	let root_pagetable = unsafe { &mut *mem::transmute::<*mut u64, *mut PageTable<PML4>>(PML4_ADDRESS.as_mut_ptr()) };
+	let root_pagetable = unsafe {
+		&mut *mem::transmute::<*mut u64, *mut PageTable<PML4>>(PML4_ADDRESS.as_mut_ptr())
+	};
 	root_pagetable.map_pages(range, PhysAddr::zero(), PageTableEntryFlags::BLANK);
 }
 
@@ -706,7 +714,9 @@ pub fn identity_map(start_address: PhysAddr, end_address: PhysAddr) {
 		last_page.address()
 	);
 
-	let root_pagetable = unsafe { &mut *mem::transmute::<*mut u64, *mut PageTable<PML4>>(PML4_ADDRESS.as_mut_ptr()) };
+	let root_pagetable = unsafe {
+		&mut *mem::transmute::<*mut u64, *mut PageTable<PML4>>(PML4_ADDRESS.as_mut_ptr())
+	};
 	let range = Page::<BasePageSize>::range(first_page, last_page);
 	let mut flags = PageTableEntryFlags::empty();
 	flags.normal().read_only().execute_disable();
