b8702113068c951d363a0ae9f5c47fd198da410c
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2021-01-02 15:46:18 +0100

    minor changes to build libhermit without PCI support

--- src/drivers/net/mod.rs
+++ src/drivers/net/mod.rs
@@ -5,6 +5,8 @@
 // http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
 // http://opensource.org/licenses/MIT>, at your option. This file may not be
 // copied, modified, or distributed except according to those terms.
+
+#[cfg(feature = "pci")]
 pub mod virtio_net;
 
 use crate::arch::kernel::percore::*;
