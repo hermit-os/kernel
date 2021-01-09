b43bf0f32c37a689bf97a52cb8036d1108535fec
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2021-01-09 07:31:45 +0100

    remove some compiler warnings

--- src/syscalls/interfaces/mod.rs
+++ src/syscalls/interfaces/mod.rs
@@ -6,6 +6,7 @@
 // http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
 // http://opensource.org/licenses/MIT>, at your option. This file may not be
 // copied, modified, or distributed except according to those terms.
+#![allow(unused)]
 
 use alloc::boxed::Box;
 use alloc::vec::Vec;
