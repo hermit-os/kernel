97880d881f7fda6fab32ef6da85a1b737dc06446
Martin Kröning <mkroening@posteo.net>
2021-05-29 19:43:23 +0200

    depr::virtio: Allow clippy::vec_box

--- src/drivers/virtio/depr/virtio.rs
+++ src/drivers/virtio/depr/virtio.rs
@@ -5,6 +5,8 @@
 // http://opensource.org/licenses/MIT>, at your option. This file may not be
 // copied, modified, or distributed except according to those terms.
 
+#![allow(clippy::vec_box)]
+
 use crate::arch::kernel::pci::{self, PciAdapter};
 
 use crate::arch::mm::paging;
