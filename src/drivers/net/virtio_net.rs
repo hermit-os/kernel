f475d539546d26cbb6d44e5975aa6dc9f5382a7b
Martin Kröning <mkroening@posteo.net>
2021-05-29 16:00:43 +0200

    Remove loops that never loop
    Fixes clippy::never_loop

--- src/drivers/net/virtio_net.rs
+++ src/drivers/net/virtio_net.rs
@@ -786,33 +786,27 @@ impl VirtioNetDriver {
 		mut caps_coll: UniCapsColl,
 		adapter: &PciAdapter,
 	) -> Result<Self, error::VirtioNetError> {
-		let com_cfg = loop {
-			match caps_coll.get_com_cfg() {
-				Some(com_cfg) => break com_cfg,
-				None => {
-					error!("No common config. Aborting!");
-					return Err(error::VirtioNetError::NoComCfg(adapter.device_id));
-				}
+		let com_cfg = match caps_coll.get_com_cfg() {
+			Some(com_cfg) => com_cfg,
+			None => {
+				error!("No common config. Aborting!");
+				return Err(error::VirtioNetError::NoComCfg(adapter.device_id));
 			}
 		};
 
-		let isr_stat = loop {
-			match caps_coll.get_isr_cfg() {
-				Some(isr_stat) => break isr_stat,
-				None => {
-					error!("No ISR status config. Aborting!");
-					return Err(error::VirtioNetError::NoIsrCfg(adapter.device_id));
-				}
+		let isr_stat = match caps_coll.get_isr_cfg() {
+			Some(isr_stat) => isr_stat,
+			None => {
+				error!("No ISR status config. Aborting!");
+				return Err(error::VirtioNetError::NoIsrCfg(adapter.device_id));
 			}
 		};
 
-		let notif_cfg = loop {
-			match caps_coll.get_notif_cfg() {
-				Some(notif_cfg) => break notif_cfg,
-				None => {
-					error!("No notif config. Aborting!");
-					return Err(error::VirtioNetError::NoNotifCfg(adapter.device_id));
-				}
+		let notif_cfg = match caps_coll.get_notif_cfg() {
+			Some(notif_cfg) => notif_cfg,
+			None => {
+				error!("No notif config. Aborting!");
+				return Err(error::VirtioNetError::NoNotifCfg(adapter.device_id));
 			}
 		};
 
