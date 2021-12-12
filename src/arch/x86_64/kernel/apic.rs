85f0190fc789b752317c52f34cf4c1afc315cc8a
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2021-12-12 11:23:13 +0100

    remove unnecessary `unsafe` block

--- src/arch/x86_64/kernel/apic.rs
+++ src/arch/x86_64/kernel/apic.rs
@@ -357,7 +357,7 @@ fn detect_from_mp() -> Result<PhysAddr, ()> {
 		Err(())
 	}?;
 
-	info!("Found MP config at 0x{:x}", unsafe { mp_float.mp_config });
+	info!("Found MP config at 0x{:x}", mp_float.mp_config);
 	info!(
 		"System uses Multiprocessing Specification 1.{}",
 		mp_float.version
