f885329034c0cf271b0e4ef1ccc535be94afa438
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2022-07-31 08:53:41 +0200

    remove compiler warnings

--- src/config.rs
+++ src/config.rs
@@ -10,6 +10,7 @@ pub(crate) const VIRTIO_MAX_QUEUE_SIZE: u16 = 2048;
 pub(crate) const VIRTIO_MAX_QUEUE_SIZE: u16 = 1024;
 
 /// Default keep alive interval in milliseconds
+#[cfg(feature = "tcp")]
 pub(crate) const DEFAULT_KEEP_ALIVE_INTERVAL: u64 = 75000;
 
 pub(crate) const HW_DESTRUCTIVE_INTERFERENCE_SIZE: usize = {
