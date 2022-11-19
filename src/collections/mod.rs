cb72df4ddc716bbfc6df6c72701cb2d3301781c3
Martin Kröning <mkroening@posteo.net>
2022-11-19 22:00:44 +0100

    Replace irqsave with hermit_sync::without_interrupts

--- src/collections/mod.rs
+++ src/collections/mod.rs
@@ -1,14 +1,1 @@
-use crate::arch::irq;
-
-/// `irqsave` guarantees that the call of the closure
-/// will be not disturbed by an interrupt
-#[inline]
-pub fn irqsave<F, R>(f: F) -> R
-where
-	F: FnOnce() -> R,
-{
-	let irq = irq::nested_disable();
-	let ret = f();
-	irq::nested_enable(irq);
-	ret
-}
+pub use hermit_sync::without_interrupts as irqsave;
