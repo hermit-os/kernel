2182a0b246503e44bf959eb01720aec301241153
Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>
2023-02-14 10:52:55 +0100

    feat: remove spinlock syscalls
    Signed-off-by: Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>

--- src/syscalls/mod.rs
+++ src/syscalls/mod.rs
@@ -16,7 +16,6 @@ pub use self::processor::*;
 #[cfg(feature = "newlib")]
 pub use self::recmutex::*;
 pub use self::semaphore::*;
-pub use self::spinlock::*;
 pub use self::system::*;
 pub use self::tasks::*;
 pub use self::timer::*;
@@ -40,7 +39,6 @@ mod recmutex;
 mod semaphore;
 #[cfg(any(feature = "tcp", feature = "udp"))]
 pub mod socket;
-mod spinlock;
 mod system;
 #[cfg(feature = "common-os")]
 pub(crate) mod table;
