8e809f376c9afdc1ba121e1b5297e6ae913908db
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2023-02-26 14:33:28 +0100

    revise debug messages

--- src/syscalls/fs.rs
+++ src/syscalls/fs.rs
@@ -1,6 +1,6 @@
 use alloc::boxed::Box;
 use alloc::collections::BTreeMap;
-use alloc::string::String;
+use alloc::string::{String, ToString};
 use alloc::vec::Vec;
 use core::ops::Deref;
 
@@ -122,7 +122,7 @@ impl Filesystem {
 				return Ok((fs.deref(), internal_path));
 			}
 
-			info!(
+			warn!(
 				"Trying to open file on non-existing mount point '{}'!",
 				mount
 			);
@@ -151,7 +151,7 @@ impl Filesystem {
 
 	/// Unlinks a file given by path
 	pub fn unlink(&mut self, path: &str) -> Result<(), FileError> {
-		info!("Unlinking file {}", path);
+		debug!("Unlinking file {}", path);
 		let (fs, internal_path) = self.parse_path(path)?;
 		fs.unlink(internal_path)?;
 		Ok(())
@@ -164,9 +164,7 @@ impl Filesystem {
 		mntpath: &str,
 		mntobj: Box<dyn PosixFileSystem + Send>,
 	) -> Result<(), ()> {
-		use alloc::borrow::ToOwned;
-
-		info!("Mounting {}", mntpath);
+		debug!("Mounting {}", mntpath);
 		if mntpath.contains('/') {
 			warn!(
 				"Trying to mount at '{}', but slashes in name are not supported!",
@@ -182,7 +180,8 @@ impl Filesystem {
 		}
 
 		// insert filesystem into mounts, done
-		self.mounts.insert(mntpath.to_owned(), mntobj);
+		self.mounts.insert(mntpath.to_string(), mntobj);
+
 		Ok(())
 	}
 
@@ -198,6 +197,8 @@ pub enum FileError {
 	ENOENT,
 	#[cfg(feature = "pci")]
 	ENOSYS,
+	#[cfg(feature = "pci")]
+	EIO,
 }
 
 pub trait PosixFileSystem {
