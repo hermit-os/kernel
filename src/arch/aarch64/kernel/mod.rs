7047ae136fc9042bda062bc7a553c1f455c155bb
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2023-03-17 19:27:24 +0100

    remove dependencies to a mboot section

--- src/arch/aarch64/kernel/mod.rs
+++ src/arch/aarch64/kernel/mod.rs
@@ -31,11 +31,7 @@ static CPU_ONLINE: TicketMutex<u32> = TicketMutex::new(0);
 global_asm!(include_str!("start.s"));
 
 /// Kernel header to announce machine features
-#[cfg_attr(
-	all(target_os = "none", not(feature = "newlib")),
-	link_section = ".data"
-)]
-#[cfg_attr(all(target_os = "none", feature = "newlib"), link_section = ".mboot")]
+#[cfg_attr(target_os = "none", link_section = ".data")]
 static mut RAW_BOOT_INFO: Option<&'static RawBootInfo> = None;
 static mut BOOT_INFO: Option<BootInfo> = None;
 
