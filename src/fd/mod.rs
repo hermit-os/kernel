61a5a1b7ea332b6e21935200a1324f7ba0f9e6f5
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2023-03-19 16:35:09 +0100

    improve debug messages

--- src/fd/mod.rs
+++ src/fd/mod.rs
@@ -167,15 +167,18 @@ fn open_flags_to_perm(flags: i32, mode: u32) -> FilePerms {
 	// mode is passed in as hex (0x777). Linux/Fuse expects octal (0o777).
 	// just passing mode as is to FUSE create, leads to very weird permissions: 0b0111_0111_0111 -> 'r-x rwS rwt'
 	// TODO: change in stdlib
-	let mode =
-		match mode {
-			0x777 => 0o777,
-			0 => 0,
-			_ => {
-				info!("Mode neither 777 nor 0, should never happen with current hermit stdlib! Using 777");
-				0o777
-			}
-		};
+	let mode = match mode {
+		0x777 => 0o777,
+		0o777 => 0o777,
+		0 => 0,
+		_ => {
+			info!(
+				"Mode {:#X} should never happen with current hermit stdlib! Using 777",
+				mode
+			);
+			0o777
+		}
+	};
 
 	let mut perms = FilePerms {
 		raw: flags as u32,
