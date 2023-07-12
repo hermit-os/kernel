c492eaa8e725734724ff6ee4a3cf7e8d4aadc4f0
Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>
2023-07-12 10:57:50 +0200

    Warn on lossy provenance casts
    Signed-off-by: Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@
 #![warn(clippy::uninlined_format_args)]
 #![warn(clippy::transmute_ptr_to_ptr)]
 #![warn(fuzzy_provenance_casts)]
+#![warn(lossy_provenance_casts)]
 #![allow(clippy::missing_safety_doc)]
 #![cfg_attr(target_arch = "aarch64", allow(incomplete_features))]
 #![cfg_attr(target_arch = "x86_64", feature(abi_x86_interrupt))]
