1c511d72ee41ca80330aaa1bc019f37981949eb6
Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>
2023-09-01 13:56:42 +0200

    refactor(xtask): collect artifact paths
    Signed-off-by: Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>

--- xtask/src/artifact.rs
+++ xtask/src/artifact.rs
@@ -45,26 +45,38 @@ impl Artifact {
 	}
 
 	pub fn builtins_archive(&self) -> Archive {
-		let mut builtins_archive = self.target_dir().to_path_buf();
-		builtins_archive.push(self.arch.hermit_triple());
-		builtins_archive.push("release");
-		builtins_archive.push("libhermit_builtins.a");
-		builtins_archive.into()
+		[
+			self.target_dir(),
+			self.arch.hermit_triple().as_ref(),
+			"release".as_ref(),
+			"libhermit_builtins.a".as_ref(),
+		]
+		.iter()
+		.collect::<PathBuf>()
+		.into()
 	}
 
 	pub fn build_archive(&self) -> Archive {
-		let mut built_archive = self.target_dir().to_path_buf();
-		built_archive.push(self.arch.triple());
-		built_archive.push(self.profile_path_component());
-		built_archive.push("libhermit.a");
-		built_archive.into()
+		[
+			self.target_dir(),
+			self.arch.triple().as_ref(),
+			self.profile_path_component().as_ref(),
+			"libhermit.a".as_ref(),
+		]
+		.iter()
+		.collect::<PathBuf>()
+		.into()
 	}
 
 	pub fn dist_archive(&self) -> Archive {
-		let mut dist_archive = self.target_dir().to_path_buf();
-		dist_archive.push(self.arch.name());
-		dist_archive.push(self.profile_path_component());
-		dist_archive.push("libhermit.a");
-		dist_archive.into()
+		[
+			self.target_dir(),
+			self.arch.name().as_ref(),
+			self.profile_path_component().as_ref(),
+			"libhermit.a".as_ref(),
+		]
+		.iter()
+		.collect::<PathBuf>()
+		.into()
 	}
 }
