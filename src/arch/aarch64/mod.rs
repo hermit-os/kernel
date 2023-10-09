e146e801106a24e0f7113ed30f13f2da1061e013
Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>
2023-10-09 10:29:27 +0200

    refactor: move memory_barrier into arch modules
    Signed-off-by: Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>

--- src/arch/aarch64/mod.rs
+++ src/arch/aarch64/mod.rs
@@ -1,2 +1,12 @@
 pub mod kernel;
 pub mod mm;
+
+/// Force strict CPU ordering, serializes load and store operations.
+#[allow(dead_code)]
+#[inline(always)]
+pub(crate) fn memory_barrier() {
+	use core::arch::asm;
+	unsafe {
+		asm!("dmb ish", options(nostack, nomem, preserves_flags),);
+	}
+}
