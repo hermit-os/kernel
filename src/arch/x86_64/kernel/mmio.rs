c8bcf1e9a49862e1cf755a2d0cb4feca9770ad73
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2023-11-02 21:58:49 +0000

    use linux arguments to determine mmio address of the NIC
    Firecracker parse the linux argument virtio_mmio.device to the
    kernel, which specifies the mmio addresses for VirtIO devices.
    This patch uses these arguments to find the network interface.wq:

--- src/arch/x86_64/kernel/mmio.rs
+++ src/arch/x86_64/kernel/mmio.rs
@@ -1,3 +1,4 @@
+use alloc::string::String;
 use alloc::vec::Vec;
 use core::{ptr, str};
 
@@ -11,6 +12,7 @@ use crate::arch::x86_64::mm::{paging, PhysAddr};
 use crate::drivers::net::virtio_net::VirtioNetDriver;
 use crate::drivers::virtio::transport::mmio as mmio_virtio;
 use crate::drivers::virtio::transport::mmio::{DevId, MmioRegisterLayout, VirtioDriver};
+use crate::env;
 
 pub const MAGIC_VALUE: u32 = 0x74726976;
 
@@ -34,9 +36,90 @@ impl MmioDriver {
 	}
 }
 
-/// Tries to find the network device within the specified address range.
-/// Returns a reference to it within the Ok() if successful or an Err() on failure.
-pub fn detect_network() -> Result<&'static mut MmioRegisterLayout, &'static str> {
+fn check_linux_args(
+	linux_mmio: &'static [String],
+) -> Result<&'static mut MmioRegisterLayout, &'static str> {
+	let virtual_address =
+		crate::arch::mm::virtualmem::allocate(BasePageSize::SIZE as usize).unwrap();
+
+	for arg in linux_mmio {
+		trace!("check linux parameter: {}", arg);
+
+		match arg.trim().trim_matches(char::from(0)).strip_prefix("4K@") {
+			Some(arg) => {
+				let v: Vec<&str> = arg.trim().split(':').collect();
+				let without_prefix = v[0].trim_start_matches("0x");
+				let current_address = usize::from_str_radix(without_prefix, 16).unwrap();
+				let irq: u32 = v[1].parse::<u32>().unwrap();
+
+				trace!(
+					"try to detect MMIO device at physical address {:#X}",
+					current_address
+				);
+
+				let mut flags = PageTableEntryFlags::empty();
+				flags.normal().writable();
+				paging::map::<BasePageSize>(
+					virtual_address,
+					PhysAddr::from(current_address.align_down(BasePageSize::SIZE as usize)),
+					1,
+					flags,
+				);
+
+				// Verify the first register value to find out if this is really an MMIO magic-value.
+				let mmio = unsafe {
+					&mut *(ptr::from_exposed_addr_mut::<MmioRegisterLayout>(
+						virtual_address.as_usize()
+							| (current_address & (BasePageSize::SIZE as usize - 1)),
+					))
+				};
+
+				let magic = mmio.get_magic_value();
+				let version = mmio.get_version();
+
+				if magic != MAGIC_VALUE {
+					trace!("It's not a MMIO-device at {mmio:p}");
+					continue;
+				}
+
+				if version != 2 {
+					trace!("Found a legacy device, which isn't supported");
+					continue;
+				}
+
+				// We found a MMIO-device (whose 512-bit address in this structure).
+				trace!("Found a MMIO-device at {mmio:p}");
+
+				// Verify the device-ID to find the network card
+				let id = mmio.get_device_id();
+
+				if id != DevId::VIRTIO_DEV_ID_NET {
+					trace!("It's not a network card at {mmio:p}");
+					continue;
+				}
+
+				info!("Found network card at {mmio:p}, irq {}", irq);
+
+				crate::arch::mm::physicalmem::reserve(
+					PhysAddr::from(current_address.align_down(BasePageSize::SIZE as usize)),
+					BasePageSize::SIZE as usize,
+				);
+
+				return Ok(mmio);
+			}
+			_ => {
+				warn!("Inavlid prefix in {}", arg);
+			}
+		}
+	}
+
+	// frees obsolete virtual memory region for MMIO devices
+	crate::arch::mm::virtualmem::deallocate(virtual_address, BasePageSize::SIZE as usize);
+
+	Err("Network card not found!")
+}
+
+fn guess_mmio() -> Result<&'static mut MmioRegisterLayout, &'static str> {
 	// Trigger page mapping in the first iteration!
 	let mut current_page = 0;
 	let virtual_address =
@@ -110,6 +193,18 @@ pub fn detect_network() -> Result<&'static mut MmioRegisterLayout, &'static str>
 	crate::arch::mm::virtualmem::deallocate(virtual_address, BasePageSize::SIZE as usize);
 
 	Err("Network card not found!")
+}
+
+/// Tries to find the network device within the specified address range.
+/// Returns a reference to it within the Ok() if successful or an Err() on failure.
+pub fn detect_network() -> Result<&'static mut MmioRegisterLayout, &'static str> {
+	let linux_mmio = env::mmio();
+
+	if linux_mmio.len() > 0 {
+		check_linux_args(linux_mmio)
+	} else {
+		guess_mmio()
+	}
 }
 
 pub(crate) fn register_driver(drv: MmioDriver) {
