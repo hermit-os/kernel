802ad8f2266a940a4d3213498ea1bd27cbf30190
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2024-02-11 10:50:52 +0100

    add interrupt handler for COM1
    To avoid debug messages of unhandled interrupts, a handler for COM1
    is registerted.

--- src/arch/x86_64/kernel/serial.rs
+++ src/arch/x86_64/kernel/serial.rs
@@ -1,5 +1,8 @@
 use x86_64::instructions::port::Port;
 
+use crate::arch::x86_64::kernel::interrupts::{self, IDT};
+use crate::arch::x86_64::kernel::{apic, COM1};
+
 enum Inner {
 	Uart(uart_16550::SerialPort),
 	Uhyve(Port<u8>),
@@ -19,6 +22,14 @@ impl SerialPort {
 		}
 	}
 
+	pub fn receive(&mut self) -> u8 {
+		if let Inner::Uart(s) = &mut self.0 {
+			s.receive()
+		} else {
+			0
+		}
+	}
+
 	pub fn send(&mut self, buf: &[u8]) {
 		match &mut self.0 {
 			Inner::Uhyve(s) => {
@@ -35,4 +46,25 @@ impl SerialPort {
 			}
 		}
 	}
+}
+
+extern "x86-interrupt" fn serial_interrupt(_stack_frame: crate::interrupts::ExceptionStackFrame) {
+	let c = unsafe { char::from_u32_unchecked(COM1.lock().as_mut().unwrap().receive().into()) };
+	if !c.is_ascii_control() {
+		print!("{}", c);
+	}
+
+	apic::eoi();
+}
+
+pub(crate) fn install_serial_interrupt() {
+	const SERIAL_IRQ: usize = 36;
+
+	unsafe {
+		let mut idt = IDT.lock();
+		idt[SERIAL_IRQ]
+			.set_handler_fn(serial_interrupt)
+			.set_stack_index(0);
+	}
+	interrupts::add_irq_name((SERIAL_IRQ - 32).try_into().unwrap(), "COM1");
 }
