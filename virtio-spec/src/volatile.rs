5cd998abd838af09768258a2df50d9be756bbabb
Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>
2024-05-29 11:03:22 +0200

    feat(virtio-spec): `impl Copy for WideVolatilePtr`
    Signed-off-by: Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>

--- virtio-spec/src/volatile.rs
+++ virtio-spec/src/volatile.rs
@@ -9,9 +9,23 @@ use crate::{be32, be64, le32, le64};
 ///
 /// In virtio, 64-bit fields are to be treated as two 32-bit fields, with low 32 bit part followed by the high 32 bit part.
 /// This type mimics [`VolatilePtr`], and allows easy access to 64-bit fields.
-pub struct WideVolatilePtr<'a, T, A> {
+pub struct WideVolatilePtr<'a, T, A>
+where
+    T: ?Sized,
+{
     low: VolatilePtr<'a, T, A>,
     high: VolatilePtr<'a, T, A>,
+}
+
+impl<'a, T, A> Copy for WideVolatilePtr<'a, T, A> where T: ?Sized {}
+
+impl<'a, T, A> Clone for WideVolatilePtr<'a, T, A>
+where
+    T: ?Sized,
+{
+    fn clone(&self) -> Self {
+        *self
+    }
 }
 
 impl<'a, T, A> WideVolatilePtr<'a, T, A> {
