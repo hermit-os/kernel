d4b3d4371c13d51ba345acc448bc90334216fc45
Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>
2024-05-29 13:19:13 +0200

    feat(virtio-spec): add `OveralignedVolatilePtr`
    Signed-off-by: Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>

--- virtio-spec/src/volatile.rs
+++ virtio-spec/src/volatile.rs
@@ -1,9 +1,11 @@
 //! Volatile Pointer Types.
 
+use core::marker::PhantomData;
+
 use volatile::access::{Readable, Writable};
 use volatile::VolatilePtr;
 
-use crate::{be32, be64, le32, le64};
+use crate::{be32, be64, le16, le32, le64, DeviceStatus};
 
 /// A wide volatile pointer for 64-bit fields.
 ///
@@ -141,4 +143,140 @@ macro_rules! impl_wide_field_access {
             )*
         }
     };
+}
+
+/// An overaligned volatile pointer for fields that require wider access operations.
+///
+/// In virtio, some fields require wider access operations than their type indicate, such as for [`mmio::DeviceRegisters`].
+///
+/// [`mmio::DeviceRegisters`]: crate::mmio::DeviceRegisters
+pub struct OveralignedVolatilePtr<'a, T, F, A>
+where
+    T: ?Sized,
+    F: ?Sized,
+{
+    ptr: VolatilePtr<'a, F, A>,
+    ty: PhantomData<VolatilePtr<'a, T, A>>,
+}
+
+impl<'a, T, F, A> Copy for OveralignedVolatilePtr<'a, T, F, A>
+where
+    T: ?Sized,
+    F: ?Sized,
+{
+}
+
+impl<'a, T, F, A> Clone for OveralignedVolatilePtr<'a, T, F, A>
+where
+    T: ?Sized,
+    F: ?Sized,
+{
+    fn clone(&self) -> Self {
+        *self
+    }
+}
+
+impl<'a, T, F, A> OveralignedVolatilePtr<'a, T, F, A>
+where
+    T: OveralignedField<F>,
+    F: Copy,
+{
+    /// Creates a new overaligned volatile pointer.
+    pub fn new(ptr: VolatilePtr<'a, F, A>) -> Self {
+        Self {
+            ptr,
+            ty: PhantomData,
+        }
+    }
+
+    /// Performs a volatile read of the contained value.
+    ///
+    /// See [`VolatilePtr::read`].
+    pub fn read(self) -> T
+    where
+        A: Readable,
+    {
+        T::from_field(self.ptr.read())
+    }
+
+    /// Performs a volatile write, setting the contained value to the given `value`.
+    ///
+    /// See [`VolatilePtr::write`].
+    pub fn write(self, value: T)
+    where
+        A: Writable,
+    {
+        self.ptr.write(value.into_field())
+    }
+
+    /// Updates the contained value using the given closure and volatile instructions.
+    ///
+    /// See [`VolatilePtr::update`].
+    pub fn update(self, f: impl FnOnce(T) -> T)
+    where
+        A: Readable + Writable,
+    {
+        let new = f(self.read());
+        self.write(new);
+    }
+}
+
+/// A trait for fields that can be accessed via [`OveralignedVolatilePtr`].
+pub trait OveralignedField<F>: private::Sealed<F> {
+    /// Converts to this type from the overaligned field.
+    fn from_field(field: F) -> Self;
+
+    /// Converts this type into the overaligned field.
+    fn into_field(self) -> F;
+}
+
+impl OveralignedField<le32> for le16 {
+    fn from_field(field: le32) -> Self {
+        field.try_into().unwrap()
+    }
+
+    fn into_field(self) -> le32 {
+        self.into()
+    }
+}
+
+impl OveralignedField<le32> for bool {
+    fn from_field(field: le32) -> Self {
+        field.to_ne() == 1
+    }
+
+    fn into_field(self) -> le32 {
+        le32::from_ne(self as u32)
+    }
+}
+
+impl OveralignedField<le32> for u8 {
+    fn from_field(field: le32) -> Self {
+        field.to_ne().try_into().unwrap()
+    }
+
+    fn into_field(self) -> le32 {
+        le32::from_ne(self.into())
+    }
+}
+
+impl OveralignedField<le32> for DeviceStatus {
+    fn from_field(field: le32) -> Self {
+        Self::from_bits_retain(u8::from_field(field))
+    }
+
+    fn into_field(self) -> le32 {
+        self.bits().into_field()
+    }
+}
+
+mod private {
+    use crate::{le16, le32, DeviceStatus};
+
+    pub trait Sealed<T> {}
+
+    impl Sealed<le32> for bool {}
+    impl Sealed<le32> for u8 {}
+    impl Sealed<le32> for le16 {}
+    impl Sealed<le32> for DeviceStatus {}
 }
