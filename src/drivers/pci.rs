77cf2b03d80fada981b2433ff24d5990ba6404b4
Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>
2024-06-10 14:19:17 +0200

    fix(pci): make BAR pages no-execute
    Signed-off-by: Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>

--- src/drivers/pci.rs
+++ src/drivers/pci.rs
@@ -202,13 +202,8 @@ impl<T: ConfigRegionAccess> PciDevice<T> {
 		// We therefore do not need to reserve any additional memory in our kernel.
 		// Map bar into RW^X virtual memory
 		let physical_address = address;
-		let virtual_address = crate::mm::map(
-			PhysAddr::from(physical_address),
-			size,
-			true,
-			false,
-			no_cache,
-		);
+		let virtual_address =
+			crate::mm::map(PhysAddr::from(physical_address), size, true, true, no_cache);
 
 		Some((virtual_address, size))
 	}
