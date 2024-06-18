525a96425994de30be5452ab054798aba381779e
Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>
2024-06-18 13:43:36 +0200

    feat(virtio-spec): add `Notification::idx`
    Signed-off-by: Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>

--- virtio-spec/src/pci.rs
+++ virtio-spec/src/pci.rs
@@ -471,3 +471,52 @@ pub struct NotificationData {
     #[bits(1)]
     pub next_wrap: u8,
 }
+
+impl NotificationData {
+    const NEXT_IDX_BITS: usize = 16;
+    const NEXT_IDX_OFFSET: usize = 16;
+
+    /// Available index
+    ///
+    /// <div class="warning">
+    ///
+    /// This collides with [`Self::next_off`] and [`Self::next_wrap`].
+    ///
+    /// </div>
+    ///
+    /// Bits: 16..32
+    pub const fn next_idx(&self) -> u16 {
+        let mask = u32::MAX >> (u32::BITS - Self::NEXT_IDX_BITS as u32);
+        let this = (le32::to_ne(self.0) >> Self::NEXT_IDX_OFFSET) & mask;
+        this as u16
+    }
+
+    /// Available index
+    ///
+    /// <div class="warning">
+    ///
+    /// This collides with [`Self::with_next_off`] and [`Self::with_next_wrap`].
+    ///
+    /// </div>
+    ///
+    /// Bits: 16..32
+    pub const fn with_next_idx(self, value: u16) -> Self {
+        let mask = u32::MAX >> (u32::BITS - Self::NEXT_IDX_BITS as u32);
+        let bits = le32::to_ne(self.0) & !(mask << Self::NEXT_IDX_OFFSET)
+            | (value as u32 & mask) << Self::NEXT_IDX_OFFSET;
+        Self(le32::from_ne(bits))
+    }
+
+    /// Available index
+    ///
+    /// <div class="warning">
+    ///
+    /// This collides with [`Self::set_next_off`] and [`Self::set_next_wrap`].
+    ///
+    /// </div>
+    ///
+    /// Bits: 16..32
+    pub fn set_next_idx(&mut self, value: u16) {
+        *self = self.with_next_idx(value);
+    }
+}
