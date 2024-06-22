313122f629ad86ddb1aaffba8ba13eb2e850e314
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2024-06-22 11:37:32 +0200

    prepare wasm support for aarch64 and riscv64

--- src/lib.rs
+++ src/lib.rs
@@ -93,7 +93,7 @@ mod shell;
 mod synch;
 pub mod syscalls;
 pub mod time;
-#[cfg(all(target_arch = "x86_64", feature = "wasm"))]
+#[cfg(all(any(target_arch = "x86_64", target_arch = "aarch64"), feature = "wasm"))]
 mod wasm;
 
 #[cfg(target_os = "none")]
@@ -158,7 +158,7 @@ extern "C" fn initd(_arg: usize) {
 	#[cfg(not(test))]
 	let (argc, argv, environ) = syscalls::get_application_parameters();
 
-	#[cfg(all(target_arch = "x86_64", feature = "wasm"))]
+	#[cfg(all(any(target_arch = "x86_64", target_arch = "aarch64"), feature = "wasm"))]
 	if crate::wasm::init().is_err() {
 		error!("Unable to initialized wasm support")
 	}
