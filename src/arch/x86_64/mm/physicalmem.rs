9aae03d33d8e1af6594ec9233256fdee032cf027
Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>
2024-10-15 17:56:43 +0200

    wip
    Signed-off-by: Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>

--- src/arch/x86_64/mm/physicalmem.rs
+++ src/arch/x86_64/mm/physicalmem.rs
@@ -28,27 +28,43 @@ fn detect_from_fdt() -> Result<(), ()> {
 
 	let mut found_ram = false;
 
-	for m in all_regions {
-		let start_address = m.starting_address as u64;
-		let size = m.size.unwrap() as u64;
-		let end_address = start_address + size;
-
-		if end_address <= mm::kernel_end_address().as_u64() {
-			continue;
-		}
-
+	if env::is_uefi() {
+		let biggest_region = all_regions.max_by_key(|m| m.size.unwrap()).unwrap();
 		found_ram = true;
 
-		let start_address = if start_address <= mm::kernel_start_address().as_u64() {
-			mm::kernel_end_address()
-		} else {
-			VirtAddr(start_address)
-		};
+		let range = PageRange::from_start_len(
+			biggest_region.starting_address.addr(),
+			biggest_region.size.unwrap(),
+		)
+		.unwrap();
 
-		let range = PageRange::new(start_address.as_usize(), end_address as usize).unwrap();
 		TOTAL_MEMORY.fetch_add(range.len().get(), Ordering::Relaxed);
 		unsafe {
 			PHYSICAL_FREE_LIST.lock().deallocate(range).unwrap();
+		}
+	} else {
+		for m in all_regions {
+			let start_address = m.starting_address as u64;
+			let size = m.size.unwrap() as u64;
+			let end_address = start_address + size;
+
+			if end_address <= mm::kernel_end_address().as_u64() {
+				continue;
+			}
+
+			found_ram = true;
+
+			let start_address = if start_address <= mm::kernel_start_address().as_u64() {
+				mm::kernel_end_address()
+			} else {
+				VirtAddr(start_address)
+			};
+
+			let range = PageRange::new(start_address.as_usize(), end_address as usize).unwrap();
+			TOTAL_MEMORY.fetch_add(range.len().get(), Ordering::Relaxed);
+			unsafe {
+				PHYSICAL_FREE_LIST.lock().deallocate(range).unwrap();
+			}
 		}
 	}
 
@@ -243,10 +259,10 @@ pub fn allocate_aligned(size: usize, align: usize) -> Result<PhysAddr, AllocErro
 /// This function must only be called from mm::deallocate!
 /// Otherwise, it may fail due to an empty node pool (POOL.maintain() is called in virtualmem::deallocate)
 pub fn deallocate(physical_address: PhysAddr, size: usize) {
-	assert!(
-		physical_address >= PhysAddr(mm::kernel_end_address().as_u64()),
-		"Physical address {physical_address:p} is not >= KERNEL_END_ADDRESS"
-	);
+	// assert!(
+	// 	physical_address >= PhysAddr(mm::kernel_end_address().as_u64()),
+	// 	"Physical address {physical_address:p} is not >= KERNEL_END_ADDRESS"
+	// );
 	assert!(size > 0);
 	assert_eq!(
 		size % BasePageSize::SIZE as usize,
