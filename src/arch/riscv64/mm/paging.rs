9db4e117dc7738f4b240a341d27b9bfd4a5a559c
Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>
2025-07-04 18:42:52 +0200

    perf(mm): make virt_to_phys a no-op again

--- src/arch/riscv64/mm/paging.rs
+++ src/arch/riscv64/mm/paging.rs
@@ -584,7 +584,7 @@ pub fn virtual_to_physical(virtual_address: VirtAddr) -> Option<PhysAddr> {
 
 #[cfg(any(feature = "fuse", feature = "vsock", feature = "tcp", feature = "udp"))]
 pub fn virt_to_phys(virtual_address: VirtAddr) -> PhysAddr {
-	virtual_to_physical(virtual_address).unwrap()
+	PhysAddr::new(virtual_address.as_u64())
 }
 
 pub fn map<S: PageSize>(
