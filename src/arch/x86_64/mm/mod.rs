483f18e7785fdd8197511588eef9c5f38e295992
Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>
2025-10-27 15:32:09 +0100

    refactor(mm): unify initialization

--- src/arch/x86_64/mm/mod.rs
+++ src/arch/x86_64/mm/mod.rs
@@ -1,20 +1,3 @@
 pub(crate) mod paging;
 
 use memory_addresses::arch::x86_64::{PhysAddr, VirtAddr};
-
-use crate::mm::{FrameAlloc, PageAlloc, PageRangeAllocator};
-
-pub unsafe fn init() {
-	unsafe {
-		paging::init();
-	}
-	unsafe {
-		FrameAlloc::init();
-	}
-	unsafe {
-		paging::log_page_tables();
-	}
-	unsafe {
-		PageAlloc::init();
-	}
-}
