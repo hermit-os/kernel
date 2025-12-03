4299cb640df62682eb5a5e65fb4c66c777eb3515
Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>
2025-12-03 13:29:55 +0100

    fix(arch): remove unused memory_barrier function

--- src/arch/aarch64/mod.rs
+++ src/arch/aarch64/mod.rs
@@ -1,11 +1,2 @@
-use aarch64_cpu::asm::barrier::{ISH, dmb};
-
 pub mod kernel;
 pub mod mm;
-
-/// Force strict CPU ordering, serializes load and store operations.
-#[allow(dead_code)]
-#[inline(always)]
-pub(crate) fn memory_barrier() {
-	dmb(ISH);
-}
