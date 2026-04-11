6fd282e59ce8ab8f07a6b256cba125557080a08d
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2026-04-11 19:29:58 +0200

    register remaining syscalls in dispatch table
    Extend SYSHANDLER_TABLE to 64 slots and register spawn, join,
    file metadata, condvar, task blocking and socket syscalls.
    Socket entries are gated behind `net`/`virtio-vsock`.
    
    Make the `addrinfo` submodule and its types `pub(crate)` so the
    table can take function pointers to `sys_getaddrinfo` and
    `sys_freeaddrinfo` without leaking private types.

--- src/syscalls/table.rs
+++ src/syscalls/table.rs
@@ -40,8 +40,96 @@ const SYSNO_WAITPID: usize = 15;
 const SYSNO_SPAWN_PROCESS: usize = 16;
 /// number of the system call `clock_gettime`
 const SYSNO_CLOCK_GETTIME: usize = 17;
+/// number of the system call `spawn`
+const SYSNO_SPAWN: usize = 18;
+/// number of the system call `spawn2`
+const SYSNO_SPAWN2: usize = 19;
+/// number of the system call `join`
+const SYSNO_JOIN: usize = 20;
+/// number of the system call `unlink`
+const SYSNO_UNLINK: usize = 21;
+/// number of the system call `mkdir`
+const SYSNO_MKDIR: usize = 22;
+/// number of the system call `rmdir`
+const SYSNO_RMDIR: usize = 23;
+/// number of the system call `stat`
+const SYSNO_STAT: usize = 24;
+/// number of the system call `lstat`
+const SYSNO_LSTAT: usize = 25;
+/// number of the system call `fstat`
+const SYSNO_FSTAT: usize = 26;
+/// number of the system call `dup`
+const SYSNO_DUP: usize = 27;
+/// number of the system call `ioctl`
+const SYSNO_IOCTL: usize = 28;
+/// number of the system call `poll`
+const SYSNO_POLL: usize = 29;
+/// number of the system call `notify`
+const SYSNO_NOTIFY: usize = 30;
+/// number of the system call `add_queue`
+const SYSNO_ADD_QUEUE: usize = 31;
+/// number of the system call `wait`
+const SYSNO_WAIT: usize = 32;
+/// number of the system call `init_queue`
+const SYSNO_INIT_QUEUE: usize = 33;
+/// number of the system call `destroy_queue`
+const SYSNO_DESTROY_QUEUE: usize = 34;
+/// number of the system call `block_current_task`
+const SYSNO_BLOCK_CURRENT_TASK: usize = 35;
+/// number of the system call `block_current_task_with_timeout`
+const SYSNO_BLOCK_CURRENT_TASK_WITH_TIMEOUT: usize = 36;
+/// number of the system call `wakeup_task`
+const SYSNO_WAKEUP_TASK: usize = 37;
+/// number of the system call `socket`
+#[cfg(any(feature = "net", feature = "virtio-vsock"))]
+const SYSNO_SOCKET: usize = 38;
+/// number of the system call `bind`
+#[cfg(any(feature = "net", feature = "virtio-vsock"))]
+const SYSNO_BIND: usize = 39;
+/// number of the system call `listen`
+#[cfg(any(feature = "net", feature = "virtio-vsock"))]
+const SYSNO_LISTEN: usize = 40;
+/// number of the system call `accept`
+#[cfg(any(feature = "net", feature = "virtio-vsock"))]
+const SYSNO_ACCEPT: usize = 41;
+/// number of the system call `connect`
+#[cfg(any(feature = "net", feature = "virtio-vsock"))]
+const SYSNO_CONNECT: usize = 42;
+/// number of the system call `recv`
+#[cfg(any(feature = "net", feature = "virtio-vsock"))]
+const SYSNO_RECV: usize = 43;
+/// number of the system call `recvfrom`
+#[cfg(any(feature = "net", feature = "virtio-vsock"))]
+const SYSNO_RECVFROM: usize = 44;
+/// number of the system call `send`
+#[cfg(any(feature = "net", feature = "virtio-vsock"))]
+const SYSNO_SEND: usize = 45;
+/// number of the system call `sendto`
+#[cfg(any(feature = "net", feature = "virtio-vsock"))]
+const SYSNO_SENDTO: usize = 46;
+/// number of the system call `shutdown`
+#[cfg(any(feature = "net", feature = "virtio-vsock"))]
+const SYSNO_SHUTDOWN: usize = 47;
+/// number of the system call `getpeername`
+#[cfg(any(feature = "net", feature = "virtio-vsock"))]
+const SYSNO_GETPEERNAME: usize = 48;
+/// number of the system call `getsockname`
+#[cfg(any(feature = "net", feature = "virtio-vsock"))]
+const SYSNO_GETSOCKNAME: usize = 49;
+/// number of the system call `getsockopt`
+#[cfg(any(feature = "net", feature = "virtio-vsock"))]
+const SYSNO_GETSOCKOPT: usize = 50;
+/// number of the system call `setsockopt`
+#[cfg(any(feature = "net", feature = "virtio-vsock"))]
+const SYSNO_SETSOCKOPT: usize = 51;
+/// number of the system call `getaddrinfo`
+#[cfg(any(feature = "net", feature = "virtio-vsock"))]
+const SYSNO_GETADDRINFO: usize = 52;
+/// number of the system call `freeaddrinfo`
+#[cfg(any(feature = "net", feature = "virtio-vsock"))]
+const SYSNO_FREEADDRINFO: usize = 53;
 /// Total number of system calls
-const NO_SYSCALLS: usize = 32;
+const NO_SYSCALLS: usize = 64;
 
 extern "C" fn invalid_syscall(sys_no: u64) -> ! {
 	error!("Invalid syscall {sys_no}");
@@ -99,6 +187,48 @@ impl SyscallTable {
 		}
 		table.handle[SYSNO_SPAWN_PROCESS] = sys_spawn_process as *const _;
 		table.handle[SYSNO_CLOCK_GETTIME] = sys_clock_gettime as *const _;
+		table.handle[SYSNO_SPAWN] = sys_spawn as *const _;
+		table.handle[SYSNO_SPAWN2] = sys_spawn2 as *const _;
+		table.handle[SYSNO_JOIN] = sys_join as *const _;
+		table.handle[SYSNO_UNLINK] = sys_unlink as *const _;
+		table.handle[SYSNO_MKDIR] = sys_mkdir as *const _;
+		table.handle[SYSNO_RMDIR] = sys_rmdir as *const _;
+		table.handle[SYSNO_STAT] = sys_stat as *const _;
+		table.handle[SYSNO_LSTAT] = sys_lstat as *const _;
+		table.handle[SYSNO_FSTAT] = sys_fstat as *const _;
+		table.handle[SYSNO_DUP] = sys_dup as *const _;
+		table.handle[SYSNO_IOCTL] = sys_ioctl as *const _;
+		table.handle[SYSNO_POLL] = sys_poll as *const _;
+		table.handle[SYSNO_NOTIFY] = sys_notify as *const _;
+		table.handle[SYSNO_ADD_QUEUE] = sys_add_queue as *const _;
+		table.handle[SYSNO_WAIT] = sys_wait as *const _;
+		table.handle[SYSNO_INIT_QUEUE] = sys_init_queue as *const _;
+		table.handle[SYSNO_DESTROY_QUEUE] = sys_destroy_queue as *const _;
+		table.handle[SYSNO_BLOCK_CURRENT_TASK] = sys_block_current_task as *const _;
+		table.handle[SYSNO_BLOCK_CURRENT_TASK_WITH_TIMEOUT] =
+			sys_block_current_task_with_timeout as *const _;
+		table.handle[SYSNO_WAKEUP_TASK] = sys_wakeup_task as *const _;
+		#[cfg(any(feature = "net", feature = "virtio-vsock"))]
+		{
+			table.handle[SYSNO_SOCKET] = crate::syscalls::socket::sys_socket as *const _;
+			table.handle[SYSNO_BIND] = crate::syscalls::socket::sys_bind as *const _;
+			table.handle[SYSNO_LISTEN] = crate::syscalls::socket::sys_listen as *const _;
+			table.handle[SYSNO_ACCEPT] = crate::syscalls::socket::sys_accept as *const _;
+			table.handle[SYSNO_CONNECT] = crate::syscalls::socket::sys_connect as *const _;
+			table.handle[SYSNO_RECV] = crate::syscalls::socket::sys_recv as *const _;
+			table.handle[SYSNO_RECVFROM] = crate::syscalls::socket::sys_recvfrom as *const _;
+			table.handle[SYSNO_SEND] = crate::syscalls::socket::sys_send as *const _;
+			table.handle[SYSNO_SENDTO] = crate::syscalls::socket::sys_sendto as *const _;
+			table.handle[SYSNO_SHUTDOWN] = crate::syscalls::socket::sys_shutdown as *const _;
+			table.handle[SYSNO_GETPEERNAME] = crate::syscalls::socket::sys_getpeername as *const _;
+			table.handle[SYSNO_GETSOCKNAME] = crate::syscalls::socket::sys_getsockname as *const _;
+			table.handle[SYSNO_GETSOCKOPT] = crate::syscalls::socket::sys_getsockopt as *const _;
+			table.handle[SYSNO_SETSOCKOPT] = crate::syscalls::socket::sys_setsockopt as *const _;
+			table.handle[SYSNO_GETADDRINFO] =
+				crate::syscalls::socket::addrinfo::sys_getaddrinfo as *const _;
+			table.handle[SYSNO_FREEADDRINFO] =
+				crate::syscalls::socket::addrinfo::sys_freeaddrinfo as *const _;
+		}
 
 		table
 	}
