3728ecd1eb3a86dc96aca97c3a43c07e20636332
Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>
2026-05-09 18:13:09 +0200

    fix(xtask): use legacy symbol mangling

--- xtask/src/build.rs
+++ xtask/src/build.rs
@@ -127,6 +127,9 @@ impl Build {
 			rustflags.push("-Zrandomize-layout");
 		}
 
+		rustflags.push("-Csymbol-mangling-version=legacy");
+		rustflags.push("-Zunstable-options");
+
 		rustflags.extend(self.cargo_build.artifact.arch.rustflags());
 
 		Ok(rustflags.join("\x1f"))
