df49d317e3c7f830214d7f23607ae0b318c957c2
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2026-05-11 13:27:10 +0000

    feat(builtins): add support for MASOS apps
    Co-authored-by: Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>

--- hermit-builtins/src/lib.rs
+++ hermit-builtins/src/lib.rs
@@ -1,9 +1,19 @@
 #![no_std]
 #![no_main]
 #![feature(linkage)]
+#![cfg_attr(feature = "masos", feature(macro_metavar_expr_concat))]
+#![cfg_attr(feature = "masos", feature(thread_local))]
 
+#[cfg(feature = "masos")]
+extern crate alloc;
+
+#[cfg(feature = "masos")]
+mod masos;
 pub mod math;
+#[cfg(feature = "masos")]
+mod alloc_impl;
 
+#[cfg(not(feature = "masos"))]
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
 	loop {}
