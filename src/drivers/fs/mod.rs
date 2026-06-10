4817cf89e422b410d5b2b98d425c5bffad44377b
Çağatay Yiğit Şahin <cyigitsahin@outlook.com>
2026-06-10 17:09:29 +0200

    refactor(virtio): deduplicate config_change in handle_interrupt

--- src/drivers/fs/mod.rs
+++ src/drivers/fs/mod.rs
@@ -161,14 +161,12 @@ impl VirtioFsDriver {
 	pub fn handle_interrupt(&mut self) {
 		let status = self.isr_stat.acknowledge();
 
-		#[cfg(not(feature = "pci"))]
-		if status.contains(virtio::mmio::InterruptStatus::CONFIGURATION_CHANGE_NOTIFICATION) {
-			info!("Configuration changes are not possible! Aborting");
-			todo!("Implement possibility to change config on the fly...")
-		}
+		let config_change = cfg_select! {
+			feature = "pci" => virtio::pci::IsrStatus::DEVICE_CONFIGURATION_INTERRUPT,
+			_ => virtio::mmio::InterruptStatus::CONFIGURATION_CHANGE_NOTIFICATION,
+		};
 
-		#[cfg(feature = "pci")]
-		if status.contains(virtio::pci::IsrStatus::DEVICE_CONFIGURATION_INTERRUPT) {
+		if status.contains(config_change) {
 			info!("Configuration changes are not possible! Aborting");
 			todo!("Implement possibility to change config on the fly...")
 		}
