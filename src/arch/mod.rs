4097f93f9a71a103a78020b0b3a5f92f238fd510
Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>
2026-06-17 18:54:20 +0200

    refactor: don't export arch modules

--- src/arch/mod.rs
+++ src/arch/mod.rs
@@ -2,15 +2,15 @@
 
 cfg_select! {
 	target_arch = "aarch64" => {
-		pub(crate) mod aarch64;
+		mod aarch64;
 		pub(crate) use self::aarch64::*;
 	}
 	target_arch = "x86_64" => {
-		pub(crate) mod x86_64;
+		mod x86_64;
 		pub(crate) use self::x86_64::*;
 	}
 	target_arch = "riscv64" => {
-		pub(crate) mod riscv64;
+		mod riscv64;
 		pub(crate) use self::riscv64::*;
 	}
 }
