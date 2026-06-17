a13e680a0541bce4ad3b85411d0a629c0b4fea4c
Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>
2026-06-17 18:54:51 +0200

    style(arch): sort cfgs

--- src/arch/mod.rs
+++ src/arch/mod.rs
@@ -5,12 +5,12 @@ cfg_select! {
 		mod aarch64;
 		pub(crate) use self::aarch64::*;
 	}
-	target_arch = "x86_64" => {
-		mod x86_64;
-		pub(crate) use self::x86_64::*;
-	}
 	target_arch = "riscv64" => {
 		mod riscv64;
 		pub(crate) use self::riscv64::*;
+	}
+	target_arch = "x86_64" => {
+		mod x86_64;
+		pub(crate) use self::x86_64::*;
 	}
 }
