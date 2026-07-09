9285a7f3c49d4657389a43292ecebedb3e1c6170
Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>
2026-07-09 12:14:13 +0200

    feat(xtask): add support for custom VMM and Hermit args

--- xtask/src/ci/firecracker.rs
+++ xtask/src/ci/firecracker.rs
@@ -5,22 +5,32 @@ use anyhow::Result;
 use clap::Args;
 use xshell::cmd;
 
+use crate::ci;
+
 /// Run image on Firecracker.
 #[derive(Args)]
 pub struct Firecracker {
 	/// Run Firecracker using `sudo`.
 	#[arg(long)]
 	sudo: bool,
+
+	/// Arguments to pass to Firecracker and Hermit, separated by another `--`.
+	#[arg(last = true)]
+	firecracker_and_hermit_args: Vec<String>,
 }
 
 impl Firecracker {
 	pub fn run(self, image: &Path, smp: usize) -> Result<()> {
 		let sh = crate::sh()?;
 
+		let (firecracker_args, hermit_args) = ci::split_args(&self.firecracker_and_hermit_args);
+		let quoted_hermit_args = shlex::try_join(hermit_args.iter().map(AsRef::as_ref))?;
+
 		let config = format!(
 			include_str!("firecracker_vm_config.json"),
 			kernel_image_path = "hermit-loader-x86_64-fc",
 			initrd_path = image.display(),
+			boot_args = quoted_hermit_args,
 			vcpu_count = smp,
 		);
 		eprintln!("firecracker config");
@@ -39,7 +49,7 @@ impl Firecracker {
 		for run in 1.. {
 			let log_path = Path::new("firecracker.log");
 			sh.write_file(log_path, "")?;
-			let res = cmd!(sh, "{program} {arg...} --no-api --config-file {config_path} --log-path {log_path} --level Info --show-level --show-log-origin").run();
+			let res = cmd!(sh, "{program} {arg...} --no-api --config-file {config_path} --log-path {log_path} --level Info --show-level --show-log-origin {firecracker_args...}").run();
 			let log = sh.read_file(log_path)?;
 
 			eprintln!("firecracker log");
