f2f053503a7f0b2d80d40283d4d30fe3e86907ca
Aelin Reidel <aelin@travitia.xyz>
2026-08-13 16:49:41 +0200

    feat(env): Parse ip= parameter
    This is heavily inspired by Linux' ip= parameter, see
    https://docs.kernel.org/admin-guide/nfs/nfsroot.html#kernel-command-line
    
    The facilities are currently unused and will be replacing HERMIT_IP and
    HERMIT_GATEWAY in a followup commit.

--- src/env/mod.rs
+++ src/env/mod.rs
@@ -1,11 +1,12 @@
 //! Inspection and manipulation of the kernel's environment.
 
+#[cfg(feature = "net")]
+mod ip_config;
 mod start_info;
 
 use alloc::borrow::{Cow, ToOwned};
 use alloc::string::String;
 use alloc::vec::Vec;
-use core::str;
 
 use ahash::RandomState;
 use hashbrown::HashMap;
@@ -13,6 +14,8 @@ use hashbrown::hash_map::Iter;
 use hermit_sync::OnceCell;
 use shlex::Shlex;
 
+#[cfg(feature = "net")]
+pub use self::ip_config::*;
 pub use self::start_info::*;
 
 static CLI: OnceCell<Cli> = OnceCell::new();
@@ -27,6 +30,8 @@ struct Cli {
 	image_path: Option<String>,
 	#[cfg(not(target_arch = "riscv64"))]
 	freq: Option<u16>,
+	#[cfg(feature = "net")]
+	default_interface_config: IpConfig,
 	env_vars: HashMap<Cow<'static, str>, String, RandomState>,
 	args: Vec<String>,
 	#[allow(dead_code)]
@@ -52,6 +57,9 @@ impl Default for Cli {
 			})
 		};
 
+		#[cfg(feature = "net")]
+		let mut default_interface_config = None;
+
 		let mut args = Vec::new();
 		let mut mmio = Vec::new();
 		while let Some(word_owned) = words.next() {
@@ -62,6 +70,28 @@ impl Default for Cli {
 				continue;
 			}
 
+			#[cfg_attr(not(feature = "net"), expect(unused_variables))]
+			if let Some(ip_config_str) = word.strip_prefix("ip=") {
+				#[cfg(feature = "net")]
+				match IpConfig::try_from(ip_config_str) {
+					Ok(config) => {
+						// This is the IP configuration for the default interface
+						// Once we support multiple interfaces, we need to support parsing multiple configurations
+						if default_interface_config.is_some() {
+							warn!("Duplicate ip= parameter passed, this is currently unsupported!");
+						}
+
+						default_interface_config = Some(config);
+					}
+					Err(e) => panic!("Could not parse configuration for default interface: {e}"),
+				}
+
+				#[cfg(not(feature = "net"))]
+				warn!("ip= parameter passed with networking support disabled, ignoring");
+
+				continue;
+			}
+
 			match word {
 				#[cfg(not(target_arch = "riscv64"))]
 				"-freq" => {
@@ -69,19 +99,25 @@ impl Default for Cli {
 					freq = Some(s.parse().unwrap());
 				}
 				"-ip" => {
-					let ip = expect_arg(words.next(), word);
-					env_vars.insert(Cow::Borrowed("HERMIT_IP"), ip);
+					// Ignore the argument value
+					drop(words.next());
+					warn!(
+						"The -ip bootarg was removed in favor of the ip= parameter and has no effect anymore"
+					);
 				}
 				"-mask" => {
 					// Ignore the argument value
 					drop(words.next());
 					warn!(
-						"The -mask bootarg was removed in favor of including the prefix length in the -ip parameter"
+						"The -mask bootarg was removed in favor of including the prefix length in the IP as part of the ip= parameter and has no effect anymore"
 					);
 				}
 				"-gateway" => {
-					let gateway = expect_arg(words.next(), word);
-					env_vars.insert(Cow::Borrowed("HERMIT_GATEWAY"), gateway);
+					// Ignore the argument value
+					drop(words.next());
+					warn!(
+						"The -gateway bootarg was removed in favor of the ip= parameter and has no effect anymore"
+					);
 				}
 				"-mount" => {
 					let gateway = expect_arg(words.next(), word);
@@ -108,6 +144,8 @@ impl Default for Cli {
 			image_path,
 			#[cfg(not(target_arch = "riscv64"))]
 			freq,
+			#[cfg(feature = "net")]
+			default_interface_config: default_interface_config.unwrap_or_default(),
 			env_vars,
 			args,
 			#[allow(dead_code)]
@@ -142,6 +180,12 @@ pub fn early_var(key: &str) -> Option<String> {
 			None
 		}
 	}
+}
+
+/// Returns the default interface IP configuration specified via ip=.
+#[cfg(feature = "net")]
+pub fn default_interface_config() -> IpConfig {
+	CLI.get().unwrap().default_interface_config
 }
 
 pub fn vars() -> Iter<'static, Cow<'static, str>, String> {
