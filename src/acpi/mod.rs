e0d7ad9444d7d2d45abbfda97eb149eaacb35752
Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>
2026-08-17 15:50:54 +0200

    refactor(x86_64/acpi): shutdown via `acpi` crate

--- src/acpi/mod.rs
+++ src/acpi/mod.rs
@@ -6,10 +6,18 @@
 mod handler;
 mod spec;
 
+use alloc::vec::Vec;
 use core::num::NonZero;
+use core::str::FromStr;
 
+use acpi::address::MappedGas;
+use acpi::aml::namespace::AmlName;
+use acpi::aml::object::{Object, WrappedObject};
 use acpi::platform::AcpiPlatform;
-use acpi::{AcpiTable, AcpiTables, Handler, PhysicalMapping, aml};
+use acpi::registers::FixedRegisters;
+use acpi::sdt::fadt::Fadt;
+use acpi::{AcpiError, AcpiTable, AcpiTables, Handler, PhysicalMapping, aml};
+use bit_field::BitField;
 use hermit_sync::OnceCell;
 
 use self::handler::AcpiHandler;
@@ -68,4 +76,46 @@ fn rsdp_paddr<H: Handler>(handler: &H) -> Option<NonZero<usize>> {
 
 	warn!("Could not find RSDP paddr.");
 	None
+}
+
+/// Enters the ACPI S5 soft off system state.
+///
+/// For details, see [16.1.7. Transitioning from the Working to the Soft Off State — 16. Waking and Sleeping — ACPI Specification 6.6 documentation].
+///
+/// [16.1.7. Transitioning from the Working to the Soft Off State — 16. Waking and Sleeping — ACPI Specification 6.6 documentation]: https://uefi.org/specs/ACPI/6.6/16_Waking_and_Sleeping.html#transitioning-from-the-working-to-the-soft-off-state
+pub fn shutdown() -> Option<!> {
+	debug!("Entering ACPI S5 soft off state...");
+
+	let aml_interpreter = AML_INTERPRETER.get()?;
+
+	// Read the S5 system state package. The contents are defined in
+	// https://uefi.org/specs/ACPI/6.6/07_Power_and_Performance_Mgmt.html#sx-system-states
+	let s5_path = AmlName::from_str(r"\_S5").ok()?;
+	let s5_object = aml_interpreter.evaluate(s5_path, Vec::new()).ok()?;
+	let Object::Package(s5) = &*s5_object else {
+		return None;
+	};
+
+	let fadt = find_table::<Fadt>()?;
+
+	let fixed_registers = FixedRegisters::new(&fadt, fadt.handler.clone()).ok()?;
+	write_pm1x_cnt(&fixed_registers.pm1_control_registers.pm1a, &s5[0]).ok()?;
+
+	None
+}
+
+fn write_pm1x_cnt<H: Handler>(
+	pm1x_cnt: &MappedGas<H>,
+	slp_typx: &WrappedObject,
+) -> Result<(), AcpiError> {
+	let slp_typx = slp_typx.as_integer().map_err(AcpiError::Aml)?;
+
+	let mut value = pm1x_cnt.read()?;
+	// SLP_TYPx
+	value.set_bits(10..13, slp_typx);
+	// SLP_EN
+	value.set_bit(13, true);
+	pm1x_cnt.write(value)?;
+
+	Ok(())
 }
