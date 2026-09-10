cf621fd305c117d93838de0bf944b692e2455f4a
Louis Vialar <louis.vialar@gmail.com>
2026-09-10 18:27:27 +0200

    perf: make DeviceAlloc phys_offset const

--- src/mm/device_alloc.rs
+++ src/mm/device_alloc.rs
@@ -43,8 +43,8 @@ unsafe impl Allocator for DeviceAlloc {
 impl DeviceAlloc {
 	/// Returns a pointer corresponding to `phys_addr`.
 	#[inline]
-	pub fn ptr_from<T>(&self, phys_addr: PhysAddr) -> *mut T {
-		let addr = phys_addr.as_usize() + self.phys_offset().as_usize();
+	pub const fn ptr_from<T>(&self, phys_addr: PhysAddr) -> *mut T {
+		let addr = phys_addr.as_usize() + const { Self.phys_offset().as_usize() };
 		ptr::with_exposed_provenance_mut(addr)
 	}
 
@@ -53,7 +53,8 @@ impl DeviceAlloc {
 	/// The address is only correct if `ptr` has been allocated by this allocator.
 	#[inline]
 	pub fn phys_addr_from<T: ?Sized>(&self, ptr: *mut T) -> PhysAddr {
-		let addr = u64::try_from(ptr.expose_provenance()).unwrap() - self.phys_offset().as_u64();
+		let addr =
+			u64::try_from(ptr.expose_provenance()).unwrap() - const { Self.phys_offset().as_u64() };
 		PhysAddr::new(addr)
 	}
 
@@ -61,11 +62,11 @@ impl DeviceAlloc {
 	///
 	/// This device allocator expects the complete physical memory to be mapped device-readable at this offset.
 	#[inline]
-	pub fn phys_offset(&self) -> VirtAddr {
+	pub const fn phys_offset(&self) -> VirtAddr {
 		if cfg!(careful) {
-			virtualmem::kernel_heap_end().as_u64().div_ceil(4).into()
+			VirtAddr::new(virtualmem::kernel_heap_end().as_u64().div_ceil(4))
 		} else {
-			0u64.into()
+			VirtAddr::new(0u64)
 		}
 	}
 }
